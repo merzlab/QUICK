28bf3d3b767fe067945135be72813c92124b9c0b
Madu86 <mmmskumara@gmail.com>
2020-07-22 06:46:04 -0700

    corrected file handling unit

--- src/subs/quick_open.f90
+++ src/subs/quick_open.f90
@@ -79,18 +79,18 @@ subroutine quick_open(funit,filename,filestat,fileform,fileacc,log_ow)
     endif 
 
     if (i /= 0) then
-        write(16,*)
-         write(16,'(a,a)') ' ERROR: Failed to overwrite file ',filename
-         call quick_exit(16,1)
+        write(2021,*)
+         write(2021,'(a,a)') ' ERROR: Failed to overwrite file ',filename
+         call quick_exit(2021,1)
     end if
     
     open(unit=funit,file=filename(k1:k2),status=fstat,form=fform,iostat=ios,position=pos)
    
    
     if (ios /= 0) then
-        write(16,*)
-        write(16,'(a,i4,a,a)') ' ERROR: Failed to open Unit=',funit, ',file name=', filename
-        call quick_exit(16,1)
+        write(2021,*)
+        write(2021,'(a,i4,a,a)') ' ERROR: Failed to open Unit=',funit, ',file name=', filename
+        call quick_exit(2021,1)
     end if
    
     if (pos /= "APPEND") rewind(funit)
