da9c30990333605903b071579ecb6c10cbfed8a7
Madu86 <mmmskumara@gmail.com>
2020-07-26 09:22:34 -0700

    renamed xc functions

--- src/octree/gpack_common.h
+++ src/octree/gpack_common.h
@@ -4,4 +4,4 @@ static const int MAX_POINTS_PER_CLUSTER = 256;
 static const int OCTREE_DEPTH = 64;
 
 //void pack_grid_pts();
-extern "C" void gpu_get_octree_info_new_imp(double *gridx, double *gridy, double *gridz, double *sigrad2, unsigned char *gpweight, unsigned int *cfweight, unsigned int *pfweight, int count);
+extern "C" void gpu_get_octree_info(double *gridx, double *gridy, double *gridz, double *sigrad2, unsigned char *gpweight, unsigned int *cfweight, unsigned int *pfweight, int count);
