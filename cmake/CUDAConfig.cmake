7063afbb5911f987fa5f0c941955d19a57238388
Chi Jin <jinchi@dev-intel16-k80.i>
2020-08-26 10:14:46 -0400

    This commit makes CMAKE to build three versions at the same time, using cmake .. -DMPI=TRUE -DCUDA_ARCH=volta -DCOMPILER=GNU

--- cmake/CUDAConfig.cmake
+++ cmake/CUDAConfig.cmake
@@ -1,5 +1,7 @@
+message("In CUDAconfig, CMAKE_CURRENT_BINARY_DIR is ${CMAKE_CURRENT_BINARY_DIR}")
+
 if(NOT ${CUDA_ARCH} STREQUAL "")
-	#SET(CUDA_SEPARABLE_COMPILATION ON) # for -dc; no need
+	set(USE_CUDA TRUE)
 	find_package(CUDA)
 	if(CUDA_FOUND)
 		message("find CUDA!!!")
@@ -7,24 +9,40 @@ if(NOT ${CUDA_ARCH} STREQUAL "")
 	
 	enable_language(CUDA)	
 	set(CMAKE_CUDA_FLAGS "-O2 -Xptxas=-v -m64 -use_fast_math")
-	set(CMAKE_CXX_FLAGS "-DCUBLAS_USE_THUNKING -O2 -I${CUDA_INCLUDE_DIRS}")
-	
-	#message("CUDA_INCLUDE_DIRS is ${CUDA_INCLUDE_DIRS}")
-	#message("CUDA_LIBRARIES  is ${CUDA_LIBRARIES}")
-	#message("CUDA_TOOLKIT_ROOT_DIR is ${CUDA_TOOLKIT_ROOT_DIR}")	#this is CUDA_HOME
-	#message("CUDA_cublas_LIBRARY is ${CUDA_cublas_LIBRARY}")	
-	
+	#set(CMAKE_CXX_FLAGS "-DCUBLAS_USE_THUNKING -O2 -I${CUDA_INCLUDE_DIRS}")
+	set(CUDA_DEVICE_CODE_FLAGS -Xptxas --disable-optimizer-constants) 	
+
 	include_directories(${CUDA_INCLUDE_DIRS})	
 
-	add_definitions(-DCUDA)
-	add_definitions(-DDEBUG)
-		
+	#add_definitions(-DCUDA)
+	#add_definitions(-DDEBUG)
+	
+	#SM7.5 = RTX20xx, RTX Titan, T4 and Quadro RTX
+	set(SM75FLAGS "-gencode arch=compute_75,code=sm_75")        
+	#SM7.0 = V100 and Volta Geforce / GTX Ampere?
+	set(SM70FLAGS "-gencode arch=compute_70,code=sm_70")
+	#SM6.0 = GP100 / P100 = DGX-1
+	set(SM60FLAGS "-gencode arch=compute_60,code=sm_60")
+	
 	if(${CUDA_ARCH} STREQUAL "pascal")
-		set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} -gencode arch=compute_60,code=sm_60")
+		set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} ${SM60FLAGS}")
 	elseif(${CUDA_ARCH} STREQUAL "volta")
-		set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} -gencode arch=compute_70,code=sm_70")
+		set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} ${SM70FLAGS}")
 	elseif(${CUDA_ARCH} STREQUAL "turing")
-		set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} -gencode arch=compute_75,code=sm_75")
+		set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} ${SM75FLAGS}")
+	elseif(${CUDA_ARCH} STREQUAL "")	#if ${CUDA_ARCH} was given from the command line, we need determine ${CMAKE_CUDA_FLAGS} from ${CMAKE_CUDA_COMPILER_VERSION}
+		if(${CMAKE_CUDA_COMPILER_VERSION} VERSION_EQUAL 8.0)
+			message(STATUS "Configuring QUICK for SM6.0")
+			set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} ${SM60FLAGS}")
+		elseif((${CMAKE_CUDA_COMPILER_VERSION} VERSION_GREATER_EQUAL 9.0) AND (${CMAKE_CUDA_COMPILER_VERSION} VERSION_LESS 10.0)) 		
+			message(STATUS "Configuring QUICK for SM6.0 and SM7.0")
+			set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} ${SM60FLAGS} ${SM70FLAGS}")
+		elseif((${CMAKE_CUDA_COMPILER_VERSION} VERSION_GREATER_EQUAL 10.0) AND (${CMAKE_CUDA_COMPILER_VERSION} VERSION_LESS_EQUAL 11.0))
+			message(STATUS "Configuring QUICK for SM6.0, SM7.0, and SM7.5")
+			set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} ${SM60FLAGS} ${SM70FLAGS} ${SM75FLAGS}")		
+		else()
+			message(FATAL_ERROR "Error: Unsupported CUDA version. ${PROJECT_NAME} requires CUDA version >= 8.0 and <= 11.0.  Please upgrade your CUDA installation or disable building with CUDA.")
+		endif()
 	endif()
 endif()
 
---
+++ cmake/ConfigModuleDirs.cmake
@@ -0,0 +1,54 @@
+#Function which (a) sets the module output directory for a target, and (b) includes other module directories
+#preserves the include directories already set on the target, adding them at the end of the list.
+
+# It removes all previous include directories pointing to Amber's modules folder,
+# so that if you use copy_target() you can set new module paths without leaving the old ones lingering
+# at the end of the include path like a bad smell.
+
+set(MODULE_DIR_NAME amber-modules)
+
+function(config_module_dirs TARGETNAME TARGET_MODULE_DIR) #3rd optional argument: extra module include directories
+
+	set(INCLUDE_DIR_EXPRESSIONS "")
+	foreach(MOD_DIR ${TARGET_MODULE_DIR} ${ARGN})
+		# add both the build dir path and the installed path
+		list(APPEND INCLUDE_DIR_EXPRESSIONS $<BUILD_INTERFACE:${CMAKE_BINARY_DIR}/${MODULE_DIR_NAME}/${MOD_DIR}> $<INSTALL_INTERFACE:include/${MODULE_DIR_NAME}/${MOD_DIR}>)
+		#message("in MOD_DIR, MOD_DIR is ${MOD_DIR}")
+		#message("in MOD_DIR, $<BUILD_INTERFACE:${CMAKE_BINARY_DIR}/${MODULE_DIR_NAME}/${MOD_DIR}>")
+		#message("in MOD_DIR, $<INSTALL_INTERFACE:include/${MODULE_DIR_NAME}/${MOD_DIR}>")
+	endforeach()
+
+	#add all of the passed module directories
+	set(INCLUDE_DIRS ${TARGET_MODULE_DIR} ${ARGN})
+		
+	# get old include directories
+	get_property(PREV_INC_DIRS TARGET ${TARGETNAME} PROPERTY INCLUDE_DIRECTORIES)
+	get_property(PREV_INT_INC_DIRS TARGET ${TARGETNAME} PROPERTY INTERFACE_INCLUDE_DIRECTORIES)
+	
+	# remove any other directories in the amber modules directory
+	set(LEFTOVER_INC_DIRS "")
+	set(LEFTOVER_INT_INC_DIRS "")
+	
+	foreach(DIR ${PREV_INC_DIRS})
+		if(NOT "${DIR}" MATCHES "${MODULE_DIR_NAME}")
+			list(APPEND LEFTOVER_INC_DIRS ${DIR})
+		endif()
+	endforeach()
+	
+	foreach(DIR ${PREV_INT_INC_DIRS})
+		if(NOT "${DIR}" MATCHES "${MODULE_DIR_NAME}")
+			list(APPEND LEFTOVER_INT_INC_DIRS ${DIR})
+		endif()
+	endforeach()
+
+	#message("finally, CMAKE_BINARY_DIR/MODULE_DIR_NAME/TARGET_MODULE_DIR is ${CMAKE_BINARY_DIR}/${MODULE_DIR_NAME}/${TARGET_MODULE_DIR}")
+	#message("")
+
+	# combine the new module dirs with the rest of the include dirs
+	set_property(TARGET ${TARGETNAME} PROPERTY Fortran_MODULE_DIRECTORY ${CMAKE_BINARY_DIR}/${MODULE_DIR_NAME}/${TARGET_MODULE_DIR})
+	set_property(TARGET ${TARGETNAME} PROPERTY INCLUDE_DIRECTORIES ${INCLUDE_DIR_EXPRESSIONS} ${LEFTOVER_INC_DIRS})
+	set_property(TARGET ${TARGETNAME} PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${INCLUDE_DIR_EXPRESSIONS} ${LEFTOVER_INT_INC_DIRS})
+	
+endfunction()
+	
+
