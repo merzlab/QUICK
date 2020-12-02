84f17eb7206d9ddf571af19a381ceeb8cf7f8c17
Madu86 <mmmskumara@gmail.com>
2020-12-02 12:58:05 -0800

    added upload_pteval function declaration, disabled smem function declaration

--- src/cuda/gpu.h
+++ src/cuda/gpu.h
@@ -45,8 +45,8 @@ extern "C" void gpu_cleanup_();
 //Following methods weddre added by Madu Manathunga
 extern "C" void gpu_upload_density_matrix_(QUICKDouble* dense);
 extern "C" void gpu_delete_dft_grid_();
-void upload_xc_smem();
-
+//void upload_xc_smem();
+void upload_pteval();
 // call subroutine
 // Fortran subroutine   --->  c interface    ->   kernel interface   ->    global       ->    kernel
 //                            [gpu_get2e]    ->      [get2e]         -> [get2e_kernel]  ->   [iclass]
