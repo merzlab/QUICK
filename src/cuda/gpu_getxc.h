96b16cce8db5f754e19844a2c672b4d9f5023cc0
Madu86 <mmmskumara@gmail.com>
2020-12-03 07:20:27 -0800

    added file to keep preprocessable functions

---
+++ src/cuda/gpu_getxc.h
@@ -0,0 +1,20 @@
+/*
+  !---------------------------------------------------------------------!
+  ! Written by Madu Manathunga on 12/03/2020                            !
+  !                                                                     ! 
+  ! Copyright (C) 2020-2021 Merz lab                                    !
+  ! Copyright (C) 2020-2021 Götz lab                                    !
+  !                                                                     !
+  ! This Source Code Form is subject to the terms of the Mozilla Public !
+  ! License, v. 2.0. If a copy of the MPL was not distributed with this !
+  ! file, You can obtain one at http://mozilla.org/MPL/2.0/.            !
+  !_____________________________________________________________________!
+
+  !---------------------------------------------------------------------!
+  ! This source file contains preprocessable functions required for     ! 
+  ! QUICK GPU version.                                                  !
+  !---------------------------------------------------------------------!
+*/
+
+
+
