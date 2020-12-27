2bbb7f7c0ad1293df4735a72b06f68ff16657021
Madu86 <mmmskumara@gmail.com>
2020-12-26 16:42:54 -0800

    disabled unnecessary operator broadcasting

--- src/scf.f90
+++ src/scf.f90
@@ -153,7 +153,6 @@ subroutine electdiis(jscf)
       call MPI_BCAST(quick_qm_struct%E,nbasis,mpi_double_precision,0,MPI_COMM_WORLD,mpierror)
       call MPI_BCAST(quick_method%integralCutoff,1,mpi_double_precision,0,MPI_COMM_WORLD,mpierror)
       call MPI_BCAST(quick_method%primLimit,1,mpi_double_precision,0,MPI_COMM_WORLD,mpierror)
-
       call MPI_BARRIER(MPI_COMM_WORLD,mpierror)
    endif
 #endif
@@ -175,6 +174,7 @@ subroutine electdiis(jscf)
       endif
    endif
 #endif
+
 
    bCalc1e = .true.
    diisdone = .false.
@@ -623,7 +623,7 @@ subroutine electdiis(jscf)
 #ifdef MPIV
       if (bMPI) then
          call MPI_BCAST(diisdone,1,mpi_logical,0,MPI_COMM_WORLD,mpierror)
-         call MPI_BCAST(quick_qm_struct%o,nbasis*nbasis,mpi_double_precision,0,MPI_COMM_WORLD,mpierror)
+!         call MPI_BCAST(quick_qm_struct%o,nbasis*nbasis,mpi_double_precision,0,MPI_COMM_WORLD,mpierror)
          call MPI_BCAST(quick_qm_struct%dense,nbasis*nbasis,mpi_double_precision,0,MPI_COMM_WORLD,mpierror)
          call MPI_BCAST(quick_qm_struct%denseOld,nbasis*nbasis,mpi_double_precision,0,MPI_COMM_WORLD,mpierror)
          call MPI_BCAST(quick_qm_struct%co,nbasis*nbasis,mpi_double_precision,0,MPI_COMM_WORLD,mpierror)
