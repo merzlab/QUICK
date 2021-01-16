a14313f08d164ba1144a5b3b6106fb1b82b1b481
Madu86 <mmmskumara@gmail.com>
2021-01-16 14:46:23 -0800

    disabled printing all timings by default

--- src/modules/quick_timer_module.f90
+++ src/modules/quick_timer_module.f90
@@ -130,16 +130,29 @@ module quick_timer_module
             call PrtAct(io,"Output Timing Information")
             write (io,'("------------- TIMING ---------------")')
             ! Initial Guess Timing
+#ifdef DEBUGTIME
             write (io,'("| INITIALIZATION TIME =",F16.9,"( ",F5.2,"%)")') timer_cumer%TInitialize, &
                 timer_cumer%TInitialize/(timer_end%TTotal-timer_begin%TTotal)*100
+#endif
             write (io,'("| INITIAL GUESS TIME  =",F16.9,"( ",F5.2,"%)")') timer_cumer%TIniGuess, &
                 timer_cumer%TIniGuess/(timer_end%TTotal-timer_begin%TTotal)*100
+
+#ifdef DEBUGTIME
+            ! Time to evaluate overlap 1e integrals
+            write (io,'("| OVERLAP 1e INTEGRALS TIME      =",F16.9,"(",F5.2,"%)")') timer_cumer%T1eS, &
+                timer_cumer%T1eS/(timer_end%TTotal-timer_begin%TTotal)*100
+            ! Time to diagnalize overlap 1e matrix
+            write (io,'("| OVERLAP 1e DIAGONALIZATION TIME =",F16.9,"(",F5.2,"%)")') timer_cumer%T1eSD, &
+                timer_cumer%T1eSD/(timer_end%TTotal-timer_begin%TTotal)*100
+#endif
+
             if(quick_method%DFT) then
                 t_tot_dftop = timer_cumer%TDFTGrdGen + timer_cumer%TDFTGrdWt + timer_cumer%TDFTGrdOct + timer_cumer%TDFTPrscrn &
                             + timer_cumer%TDFTGrdPck
                 ! Total time for dft grid formation, pruning and prescreening
                 write (io,'("| DFT GRID OPERATIONS =",F16.9,"( ",F5.2,"%)")') t_tot_dftop, &
                 (t_tot_dftop)/(timer_end%TTotal-timer_begin%TTotal)*100
+#ifdef DEBUGTIME
                 ! Time for grid formation
                 write (io,'("| ",6x,"TOTAL GRID FORMATION TIME   =",F16.9,"( ",F5.2,"%)")') timer_cumer%TDFTGrdGen, &
                 timer_cumer%TDFTGrdGen/(timer_end%TTotal-timer_begin%TTotal)*100
@@ -155,23 +168,26 @@ module quick_timer_module
                 ! Time for packing points
                 write (io,'("| ",6x,"TOTAL DATA PACKING TIME     =",F16.9,"( ",F5.2,"%)")')timer_cumer%TDFTGrdPck, &
                 timer_cumer%TDFTGrdPck/(timer_end%TTotal-timer_begin%TTotal)*100
+#endif
             endif
-#ifdef CUDA_MPIV
+#if defined CUDA_MPIV && DEBUGTIME
             t_tot_lb=timer_cumer%t2elb+timer_cumer%tdftlb+timer_cumer%tdftrb
             write (io,'("| TOTAL LOAD BALANCING TIME =",F16.9,"( ",F5.2,"%)")') t_tot_lb, &
             t_tot_lb/(timer_end%TTotal-timer_begin%TTotal)*100
+#ifdef DEBUGTIME
             write (io,'("| ",6x,"2E LOAD BALANCING TIME =",F16.9,"( ",F5.2,"%)")') timer_cumer%t2elb, &
             timer_cumer%t2elb/(timer_end%TTotal-timer_begin%TTotal)*100
             if(quick_method%DFT) then
                 write (io,'("| ",6x,"DFT LOAD BALANCING TIME =",F16.9,"( ",F5.2,"%)")') timer_cumer%tdftlb, &
                 timer_cumer%tdftlb/(timer_end%TTotal-timer_begin%TTotal)*100
                 if (quick_method%opt .or. quick_method%grad ) then
-                   write (io,'("| ",6x,"DFT GRID REPRUNING TIME =",F16.9,"( ",F5.2,"%)")') timer_cumer%tdftpg, &
-                   timer_cumer%tdftpg/(timer_end%TTotal-timer_begin%TTotal)*100
-                   write (io,'("| ",12x,"DFT LOAD REBALANCING TIME =",F16.9,"( ",F5.2,"%)")') timer_cumer%tdftrb, &
+!                   write (io,'("| ",6x,"DFT GRID REPRUNING TIME =",F16.9,"( ",F5.2,"%)")') timer_cumer%tdftpg, &
+!                   timer_cumer%tdftpg/(timer_end%TTotal-timer_begin%TTotal)*100
+                   write (io,'("| ",6x,"DFT LOAD REBALANCING TIME =",F16.9,"( ",F5.2,"%)")') timer_cumer%tdftrb, &
                    timer_cumer%tdftrb/(timer_end%TTotal-timer_begin%TTotal)*100
                 endif
             endif
+#endif
 #endif
             if (quick_method%nodirect) &
             write (io,'("| 2E EVALUATION TIME =",F16.9,"( ",F5.2,"%)")') timer_end%T2eAll-timer_begin%T2eAll, &
@@ -182,21 +198,17 @@ module quick_timer_module
             ! Time to evaluate operator
             write (io,'("| ",6x,"TOTAL OP TIME      =",F16.9,"( ",F5.2,"%)")') timer_cumer%TOp, &
                 timer_cumer%TOp/(timer_end%TTotal-timer_begin%TTotal)*100
-            ! Time to evaluate overlap 1e integrals
-            write (io,'("| ",12x,"OVERLAP 1e INTEGRALS TIME      =",F16.9,"( ",F5.2,"%)")') timer_cumer%T1eS, &
-                timer_cumer%T1eS/(timer_end%TTotal-timer_begin%TTotal)*100
-            ! Time to diagnalize overlap 1e matrix
-            write (io,'("| ",12x,"OVERLAP 1e DIAGONALIZATION TIME =",F16.9,"( ",F5.2,"%)")') timer_cumer%T1eSD, &
-                timer_cumer%T1eSD/(timer_end%TTotal-timer_begin%TTotal)*100
             ! Time to evaluate 1e integrals
             write (io,'("| ",12x,"TOTAL 1e TIME      =",F16.9,"( ",F5.2,"%)")') timer_cumer%T1e, &
                 timer_cumer%T1e/(timer_end%TTotal-timer_begin%TTotal)*100
+#ifdef DEBUGTIME
             ! Time to evaluate kinetic 1e integrals
             write (io,'("| ",15x,"KINETIC 1e INTEGRALS TIME    =",F16.9,"( ",F5.2,"%)")') timer_cumer%T1eT, &
                 timer_cumer%T1eT/(timer_end%TTotal-timer_begin%TTotal)*100
             ! Time to evaluate attraction 1e integrals
             write (io,'("| ",15x,"ATTRACTION 1e INTEGRALS TIME =",F16.9,"( ",F5.2,"%)")') timer_cumer%T1eV, &
                 timer_cumer%T1eV/(timer_end%TTotal-timer_begin%TTotal)*100
+#endif
             ! Time to evaluate 2e integrals
             write (io,'("| ",12x,"TOTAL 2e TIME      =",F16.9,"( ",F5.2,"%)")') timer_cumer%T2e, &
                 timer_cumer%T2e/(timer_end%TTotal-timer_begin%TTotal)*100
@@ -205,13 +217,15 @@ module quick_timer_module
                 write (io,'("| ",12x,"TOTAL EXC TIME     =",F16.9,"( ",F5.2,"%)")') timer_cumer%TEx, &
                     timer_cumer%TEx/(timer_end%TTotal-timer_begin%TTotal)*100
             endif
-#ifdef MPIV
+#if defined MPIV && DEBUGTIME
             ! time to reduce operator
             write (io,'("| ",12x,"TOTAL OPERATOR REDUCTION TIME     =",F16.9,"( ",F5.2,"%)")') timer_cumer%TEred, &
                     timer_cumer%TEred/(timer_end%TTotal-timer_begin%TTotal)*100
 #endif
+#ifdef DEBUGTIME
             write (io,'("| ",12x,"TOTAL ENERGY TIME  =",F16.9,"( ",F5.2,"%)")') timer_cumer%TE, &
                 timer_cumer%TE/(timer_end%TTotal-timer_begin%TTotal)*100
+#endif
             ! DII Time
             write (io,'("| ",6x,"TOTAL DII TIME      =",F16.9,"( ",F5.2,"%)")') timer_cumer%TDII, &
                 timer_cumer%TDII/(timer_end%TTotal-timer_begin%TTotal)*100
@@ -224,28 +238,23 @@ module quick_timer_module
                     timer_cumer%TDiag/(timer_end%TTotal-timer_begin%TTotal)*100
             endif
 
-            if (quick_method%dipole) then
-            ! Dipole Timing
-                write (io,'("| ",6x,"DIPOLE TIME        =",F16.9,"( ",F5.2,"%)")') timer_cumer%TDip, &
-                    timer_cumer%TDip/(timer_end%TTotal-timer_begin%TTotal)*100
-            endif
             ! Grad Timing
             if (quick_method%opt .or. quick_method%grad ) then
                 write (io,'("| TOTAL GRADIENT TIME      =",F16.9,"( ",F5.2,"%)")') timer_cumer%TGrad, &
                     timer_cumer%TGrad/(timer_end%TTotal-timer_begin%TTotal)*100
-
+#ifdef DEBUGTIME
                 write (io,'("| ",6x,"TOTAL NUCLEAR GRADIENT TIME =",F16.9,"( ",F5.2,"%)")') timer_cumer%TNucGrad, &
                         timer_cumer%TNucGrad/(timer_end%TTotal-timer_begin%TTotal)*100
-
+#endif
                 write (io,'("| ",6x,"TOTAL 1e GRADIENT TIME      =",F16.9,"( ",F5.2,"%)")') timer_cumer%T1eGrad, &
                         timer_cumer%T1eGrad/(timer_end%TTotal-timer_begin%TTotal)*100
-
+#ifdef DEBUGTIME
                 write (io,'("| ",9x,"KINETIC 1e GRADIENT TIME      =",F16.9,"( ",F5.2,"%)")') timer_cumer%T1eTGrad, &
                         timer_cumer%T1eTGrad/(timer_end%TTotal-timer_begin%TTotal)*100
 
                 write (io,'("| ",9x,"ATTRACTION 1e GRADIENT TIME   =",F16.9,"( ",F5.2,"%)")') timer_cumer%T1eVGrad, &
                         timer_cumer%T1eVGrad/(timer_end%TTotal-timer_begin%TTotal)*100
-
+#endif
                 write (io,'("| ",6x,"TOTAL 2e GRADIENT TIME      =",F16.9,"( ",F5.2,"%)")') timer_cumer%T2eGrad, &
                         timer_cumer%T2eGrad/(timer_end%TTotal-timer_begin%TTotal)*100
 
@@ -253,7 +262,7 @@ module quick_timer_module
                    write (io,'("| ",6x,"TOTAL EXC GRADIENT TIME     =",F16.9,"( ",F5.2,"%)")') timer_cumer%TExGrad, &
                            timer_cumer%TExGrad/(timer_end%TTotal-timer_begin%TTotal)*100
                 endif
-#ifdef MPIV
+#if defined MPIV && DEBUGTIME
                 write (io,'("| ",6x,"TOTAL GRAD REDUCTION TIME   =",F16.9,"( ",F5.2,"%)")') timer_cumer%TGradred, &
                         timer_cumer%TGradred/(timer_end%TTotal-timer_begin%TTotal)*100
 #endif
@@ -264,6 +273,14 @@ module quick_timer_module
                 write (io,'("| TOTAL MP2 TIME     =",F16.9,"( ",F5.2,"%)")') timer_cumer%TMP2, &
                     timer_cumer%TMP2/(timer_end%TTotal-timer_begin%TTotal)*100
             endif
+
+#ifdef DEBUGTIME
+            if (quick_method%dipole) then
+            ! Dipole Timing
+                write (io,'("| DIPOLE TIME        =",F16.9,"(",F5.2,"%)")') timer_cumer%TDip, &
+                    timer_cumer%TDip/(timer_end%TTotal-timer_begin%TTotal)*100
+            endif
+#endif
 
             ! Most Important, total time
             write (io,'("| TOTAL TIME          =",F16.9)') timer_end%TTotal-timer_begin%TTotal
@@ -288,6 +305,7 @@ module quick_timer_module
   call MPI_GATHER(timer_cumer%T2eGrad,1,mpi_double_precision,t2egrad,1,mpi_double_precision,0,MPI_COMM_WORLD,mpierror)  
   call MPI_GATHER(timer_cumer%TExGrad,1,mpi_double_precision,txcgrad,1,mpi_double_precision,0,MPI_COMM_WORLD,mpierror)
 
+#ifdef DEBUGTIME
   if(master) then
     write (io,'(" ")')
     write (io,'("----------- MPI TIMING -------------")')
@@ -307,7 +325,7 @@ module quick_timer_module
      "XC GRAD TIME  =",F16.9)') i, t2e(i), txc(i), t2egrad(i), txcgrad(i) 
     enddo
   endif
-
+#endif
 #endif
 
 
