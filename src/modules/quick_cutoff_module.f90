385954792b9aeba742fe81afe1ae6bfe2da26af8
Madu86 <mmmskumara@gmail.com>
2021-01-26 15:50:14 -0800

    created new module for schwarz subroutines

---
+++ src/modules/quick_cutoff_module.f90
@@ -0,0 +1,25 @@
+!---------------------------------------------------------------------!
+! Created by Madu Manathunga on 01/26/2021                            !
+!                                                                     !
+! Previous contributors: Yipu Miao, Xio He, Alessandro Genoni,        !
+!                         Ken Ayers & Ed Brothers                     !
+!                                                                     !
+! Copyright (C) 2020-2021 Merz lab                                    !
+! Copyright (C) 2020-2021 Götz lab                                    !
+!                                                                     !
+! This Source Code Form is subject to the terms of the Mozilla Public !
+! License, v. 2.0. If a copy of the MPL was not distributed with this !
+! file, You can obtain one at http://mozilla.org/MPL/2.0/.            !
+!_____________________________________________________________________!
+
+! Xiao HE 07/07/07
+! Schwartz cutoff is implemented here. (ab|cd)**2<=(ab|ab)*(cd|cd)
+! Reference: Strout DL and Scuseria JCP 102(1995),8448.
+
+module quick_cutoff_module
+  implicit double precision(a-h,o-z)
+  private
+
+contains
+
+end module quick_cutoff_module
