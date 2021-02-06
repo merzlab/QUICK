c33096b5a5708dfc5435662fd06c3da486b0a264
Madu86 <mmmskumara@gmail.com>
2021-02-05 16:04:49 -0800

    added wrappers for MIRP FmT function

---
+++ src/subs/mirp_fmt.cpp
@@ -0,0 +1,36 @@
+/*
+  !---------------------------------------------------------------------!
+  ! Written by Madu Manathunga on 02/05/2021                            !
+  !                                                                     ! 
+  ! Copyright (C) 2020-2021 Merz lab                                    !
+  ! Copyright (C) 2020-2021 Götz lab                                    !
+  !                                                                     !
+  ! This Source Code Form is subject to the terms of the Mozilla Public !
+  ! License, v. 2.0. If a copy of the MPL was not distributed with this !
+  ! file, You can obtain one at http://mozilla.org/MPL/2.0/.            !
+  !_____________________________________________________________________!
+
+  !---------------------------------------------------------------------!
+  ! This source file contains wrapper functions required to use FmT     !
+  ! function from MIRP library.                                         !
+  !                                                                     ! 
+  !---------------------------------------------------------------------!
+*/
+
+#include <mirp/kernels/boys.h>
+#include <mirp/math.h>
+
+#include <iostream>
+#include "mirp_fmt.hpp"
+
+// Fortran accessible fmt function from mirp library
+void mirp_fmt_(int *m, double *t, double *fmt){
+
+  double *F = new double[*m+1]();
+
+  mirp_boys_exact(F, *m, *t);
+
+  for(int i=0; i<=*m+1; ++i) fmt[i]=F[i];
+
+}
+
