5fc3e078269f8bb183550e7013ff8329ec6bb6f7
Madu86 <mmmskumara@gmail.com>
2021-02-25 09:14:01 -0800

    added separate module for exception handling

---
+++ src/modules/quick_exception_module.f90
@@ -0,0 +1,25 @@
+!---------------------------------------------------------------------!
+! Created by Madu Manathunga on 04/16/2020                            !
+!                                                                     !
+! Copyright (C) 2020-2021 Merz lab                                    !
+! Copyright (C) 2020-2021 Götz lab                                    !
+!                                                                     !
+! This Source Code Form is subject to the terms of the Mozilla Public !
+! License, v. 2.0. If a copy of the MPL was not distributed with this !
+! file, You can obtain one at http://mozilla.org/MPL/2.0/.            !
+!_____________________________________________________________________!
+
+!---------------------------------------------------------------------!
+! This module performs exception handling in QUICK.                   !
+!_____________________________________________________________________!
+
+module quick_exception_module
+
+  implicit none
+  private
+
+contains
+
+
+
+end module quick_exception_module
