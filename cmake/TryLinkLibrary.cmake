4783ca33dd76f7aafb179ba2b4d07a15e1fa56db
Jamie <jsmith@crackofdawn.onmicrosoft.com>
2021-02-26 01:58:30 -0800

    Fix some compile issues with PGI

--- cmake/TryLinkLibrary.cmake
+++ cmake/TryLinkLibrary.cmake
@@ -152,6 +152,7 @@ int main(int argc, char** args)
 
   if(MISSING_LIBRARIES)
     set(${RESULT_VAR} FALSE PARENT_SCOPE)
+    message(STATUS "Looking for ${TLL_FUNCTION} in${LIBRARY_MESSAGE_TEXT} - missing prerequisites")
     return()
   endif()
 
--- cmake/hanjianwei/FindMKL.cmake
+++ cmake/hanjianwei/FindMKL.cmake
@@ -194,7 +194,7 @@ if(MKL_MULTI_THREADED)
 	find_package(OpenMP)
 	
 	# NOTE: we don't want to link against the imported targets, because that would apply OpenMP compile flags to anything linked to MKL
-	set(MKL_OMP_LIBRARY ${OpenMP_C_OPTIONS} ${OpenMP_C_LIBRARIES})
+	set(MKL_OMP_LIBRARY ${OpenMP_C_FLAGS})
 else()
 	set(MKL_OMP_LIBRARY "")
 endif()
@@ -277,7 +277,7 @@ endif()
 find_package(CMath)
 list(APPEND MKL_NEEDED_LIBNAMES CMath_FOUND)
 
-# Build the final library lists    
+# Build the final library lists  
 set(MKL_LIBRARIES ${LIB_LIST_PREFIX} mkl::core mkl::threading mkl::interface ${LIB_LIST_SUFFIX} ${MKL_LIBDL} ${MKL_PTHREAD_LIB} ${MKL_OMP_LIBRARY} C::Math)
 set(MKL_FORTRAN_LIBRARIES ${LIB_LIST_PREFIX} mkl::core mkl::threading mkl::fortran_interface ${LIB_LIST_SUFFIX} ${MKL_LIBDL} ${MKL_PTHREAD_LIB} ${MKL_OMP_LIBRARY} C::Math)
 
