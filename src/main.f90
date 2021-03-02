bae859126fd1616cb8445770507e6803c3abd3bb
Madu86 <mmmskumara@gmail.com>
2021-03-01 17:44:36 -0800

    initialized ierr to zero

--- src/main.f90
+++ src/main.f90
@@ -49,6 +49,7 @@
     ! 1. The first thing that must be done is to initialize and prepare files
     !------------------------------------------------------------------
     ! Initial neccessary variables
+    ierr=0
     call initialize1(ierr)
     !-------------------MPI/MASTER---------------------------------------
     masterwork_readInput: if (master) then
