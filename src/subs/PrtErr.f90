94516c44c20519f08efeec4e56baf7f2c6f7796e
Madu86 <mmmskumara@gmail.com>
2021-03-07 12:27:51 -0800

    updated text

--- src/subs/PrtErr.f90
+++ src/subs/PrtErr.f90
@@ -21,7 +21,7 @@ subroutine PrtErr(io,line)
   leng=len(line)
   L=0
   write(io,'(a)')
-  write(io,'("| ERROR: ",a)') line
+  write(io,'("| Error: ",a)') line
   write(io,'(a)')
   call flush(io)
   return
