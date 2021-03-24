b61b8ccd3ec47c64226854bc863bd5272386148a
Madu86 <mmmskumara@gmail.com>
2021-03-24 14:39:36 -0700

    added preprocessable eri module

---
+++ src/modules/quick_eri_module.f90
@@ -0,0 +1,30 @@
+!---------------------------------------------------------------------!
+! Created by Madu Manathunga on 03/24/2021                            !
+!                                                                     !
+! Previous contributors: Yipu Miao, Xio He, Alessandro Genoni,        !
+!                         Ken Ayers & Ed Brothers                     !
+!                                                                     ! 
+! Copyright (C) 2020-2021 Merz lab                                    !
+! Copyright (C) 2020-2021 Götz lab                                    !
+!                                                                     !
+! This Source Code Form is subject to the terms of the Mozilla Public !
+! License, v. 2.0. If a copy of the MPL was not distributed with this !
+! file, You can obtain one at http://mozilla.org/MPL/2.0/.            !
+!_____________________________________________________________________!
+
+#ifdef OSHELL
+module quick_oshell_eri_module
+#else
+module quick_cshell_eri_module
+#endif
+
+
+contains
+
+
+#ifdef OSHELL
+end module quick_oshell_eri_module
+#else
+end module quick_cshell_eri_module
+#endif
+
