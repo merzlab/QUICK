687cf2876a3e24a6c188f1107518530eea3a2034
Madu86 <mmmskumara@gmail.com>
2021-03-26 13:29:19 -0700

    created separate module for dft stuff

---
+++ src/modules/quick_dft_module.f90
@@ -0,0 +1,24 @@
+!---------------------------------------------------------------------!
+! Created by Madu Manathunga on 03/26/2021                            !
+!                                                                     ! 
+! Copyright (C) 2020-2021 Merz lab                                    !
+! Copyright (C) 2020-2021 Götz lab                                    !
+!                                                                     !
+! This Source Code Form is subject to the terms of the Mozilla Public !
+! License, v. 2.0. If a copy of the MPL was not distributed with this !
+! file, You can obtain one at http://mozilla.org/MPL/2.0/.            !
+!_____________________________________________________________________!
+
+! This module should contain stuff relevent for DFT
+module quick_dft_module
+
+  implicit none
+  private
+
+  public :: 
+
+contains
+
+
+
+end module quick_dft_module
