cec100b0d21398da6592b0ab5a01378417e2c4b7
Madu86 <mmmskumara@gmail.com>
2021-03-26 14:01:15 -0700

    disabled calling udft and usedft

--- src/uscf.f90
+++ src/uscf.f90
@@ -86,8 +86,8 @@
     ! 1)  Form alpha operator matrix.
 
         if (quick_method%HF) call uhfoperatora
-        if (quick_method%DFT) call udftoperatora
-        if (quick_method%SEDFT) call usedftoperatora
+    !    if (quick_method%DFT) call udftoperatora
+    !    if (quick_method%SEDFT) call usedftoperatora
 
        ! call shift(sval,.true.,jscf)
 
@@ -192,8 +192,8 @@
     ! 1)  Form beta operator matrix.
 
         if (quick_method%HF) call uhfoperatorb
-        if (quick_method%DFT) call udftoperatorb
-        if (quick_method%SEDFT) call usedftoperatorb
+    !    if (quick_method%DFT) call udftoperatorb
+    !    if (quick_method%SEDFT) call usedftoperatorb
 
        ! call shift(sval,.false.,jscf)
 
