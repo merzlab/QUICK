5d9fb801c45469101c66076e083e0721e112aaf6
Madu86 <mmmskumara@gmail.com>
2021-04-06 12:26:31 -0700

    cleaned up

--- src/libxc/gpu.h
+++ src/libxc/gpu.h
@@ -5,11 +5,8 @@
 extern "C" {
 #endif
 extern int dryrun;
-//extern void *unk_gpu_func_params;
 
-//typedef void(*point_kernel)(const void*,  xc_gga_work_x_t*);
-
-//Struct to hold gga_x parameters on gpu
+//Structs to hold work parameters in gpu version
 typedef struct{
         double sfact, dens_threshold, alpha, beta, c_zk0, c_vrho0, c_vrho1, c_vrho2, c_vsigma0, c_vsigma1;
 	int ggax_maple2c_psize;
@@ -30,20 +27,9 @@ typedef struct{
         int k_index;
 }gpu_lda_work_params;
 
-//Define a parent struct to carry all the above structs
-/*typedef struct{
-	int func_id;
-	gpu_ggax_work_params gwp; //Parameters required for gpu work function
-	void *gpu_func_params; //parameters required for maple2c codes
-}gpu_libxc_d_s;
-*/
-
-//holds the parent struct containing all libxc params 
-//extern gpu_libxc_d_s* glds;
 void get_gpu_work_params(xc_func_type* p, void *gpu_work_params);
 
-//This will load gga_x work parameters into a gpu_ggax_work_params struct variable and return it. 
-
+//These will load work parameters into struct variables. 
 void set_gpu_ggax_work_params(double sfact, double dens_threshold, double alpha,                
         double beta, double c_zk0, double c_vrho0, double c_vrho1, double c_vrho2, double c_vsigma0, double c_vsigma1, int k_index, gpu_ggax_work_params *w);
 
