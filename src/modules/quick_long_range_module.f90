881827fdcf5e7ffcecb8ac0fca9f271719964de4
Madu86 <mmmskumara@gmail.com>
2021-04-26 09:47:39 -0700

    more on long range integrals

--- src/modules/quick_long_range_module.f90
+++ src/modules/quick_long_range_module.f90
@@ -64,8 +64,11 @@ contains
     double precision, intent(in) :: Zc, RC(3)
 
     integer :: M, LL, NII1, NII2, NJJ1, NJJ2, NKK1, NKK2, NLL1, NLL2, NNAB, NNCD, NABCDTYPE, &
-               NNA, NNC, NABCD, ITT, JJJ, III
-    double precision :: RA(3),RB(3),RD(3)
+               NNA, NNC, NABCD, ITT, JJJ, III, Nprij, Nprii, iitemp, I1, I2 
+    double precision :: RA(3),RB(3),RD(3), P(3), AAtemp(3), Ptemp(3), Q(3), W(3), WQtemp(3), &
+                        WPtemp(3), FM(0:13)
+    double precision :: AA, AB, ABtemp, cutoffprim1, cutoffprim, CD, ABCD, ROU, RPQ, ABCDsqrt, &
+                        ABcom, CDcom, XXXtemp, Qtemp, T
 
     ! set the coordinates of shell centers
     do M=1,3
@@ -101,7 +104,155 @@ contains
     NABCD=NII2+NJJ2+NKK2+NLL2
     ITT=0
 
-    
+    do JJJ=1,quick_basis%kprim(JJ)
+      Nprij=quick_basis%kstart(JJ)+JJJ-1
+
+      ! the second cycle is for i prim
+      ! II and NpriI are the tracking indices
+      do III=1,quick_basis%kprim(II)
+        Nprii=quick_basis%kstart(II)+III-1
+
+        !For NpriI and NpriJ primitives, we calculate the following quantities
+        AB=Apri(Nprii,Nprij)    ! AB = Apri = expo(NpriI)+expo(NpriJ). Eqn 8 of HGP.
+        ABtemp=0.5d0/AB         ! ABtemp = 1/(2Apri) = 1/2(expo(NpriI)+expo(NpriJ))
+
+        ! compute this for screening integrals further 
+        cutoffprim1=dnmax*cutprim(Nprii,Nprij)
+
+        do M=1,3
+           !Eqn 9 of HGP
+           ! P' is the weighting center of NpriI and NpriJ
+           !                           --->           --->
+           ! ->  ------>       expo(I)*xyz(I)+expo(J)*xyz(J)
+           ! P = P'(I,J)  = ------------------------------
+           !                       expo(I) + expo(J)
+           P(M)=Ppri(M,Nprii,Nprij)
+
+           !Multiplication of Eqns 9  by Eqn 8 of HGP.. 
+           !                        -->            -->
+           ! ----->         expo(I)*xyz(I)+expo(J)*xyz(J)                                 -->            -->
+           ! AAtemp = ----------------------------------- * (expo(I) + expo(J)) = expo(I)*xyz(I)+expo(J)*xyz(J)
+           !                  expo(I) + expo(J)
+           AAtemp(M)=P(M)*AB
+
+           !Requires for HGP Eqn 6. 
+           ! ----->   ->  ->
+           ! Ptemp  = P - A
+           Ptemp(M)=P(M)-RA(M)
+        enddo
+        
+        ! Since the Ket vector is composed of a single and a null gaussian, we do the following.
+        LLL=1
+        KKK=1
+        
+        ! We no longer have Nprik, Npril. Omit the primitive integral screening for now.
+        !cutoffprim=cutoffprim1*cutprim(Nprik,Npril)
+        !if(cutoffprim.gt.quick_method%primLimit)then
+          
+          ! Nita quantity of HGP Eqn 10. This is same as zita (AB) above. 
+          ! CD=expo(C)+expo(D) where the first term on R.H.S. will come from York group, the second is zero.
+          CD=Zc
+
+          !First term of HGP Eqn 12 without sqrt.
+          ABCD=AB+CD            ! ABCD = expo(NpriI)+expo(NpriJ)+expo(NpriK)+expo(NpriL)
+
+          !First term of HGP Eqn 13.
+          !         AB * CD      (expo(I)+expo(J))*(expo(K)+expo(L))
+          ! Rou = ----------- = ------------------------------------
+          !         AB + CD         expo(I)+expo(J)+expo(K)+expo(L)
+          ROU=AB*CD/ABCD
+
+          RPQ=0.0d0
+
+          !First term of HGP Eqn 12 with sqrt. 
+          !              _______________________________
+          ! ABCDsqrt = \/expo(I)+expo(J)+expo(K)+expo(L)
+          ABCDsqrt=dsqrt(ABCD)
+
+          CDtemp=0.5d0/CD       ! CDtemp =  1/2(expo(NpriK)+expo(NpriL))
+
+          !These terms are required for HGP Eqn 6.
+          !                expo(I)+expo(J)                        expo(K)+expo(L)
+          ! ABcom = --------------------------------  CDcom = --------------------------------
+          !          expo(I)+expo(J)+expo(K)+expo(L)           expo(I)+expo(J)+expo(K)+expo(L)          
+          ABcom=AB/ABCD
+          CDcom=CD/ABCD
+
+          ! ABCDtemp = 1/2(expo(I)+expo(J)+expo(K)+expo(L))
+          ABCDtemp=0.5d0/ABCD
+
+          do M=1,3
+            ! Calculate Q of HGP 10, which is same as P above. 
+            ! Q' is the weighting center of NpriK and NpriL
+            !                           --->           --->
+            ! ->  ------>       expo(K)*xyz(K)+expo(L)*xyz(L)
+            ! Q = P'(K,L)  = ------------------------------
+            !                       expo(K) + expo(L)
+            ! But since expo(L) is zero, this reduces to the following.
+            Q(M)=RC(M)         
+
+            ! HGP Eqn 10. 
+            ! W' is the weight center for NpriI,NpriJ,NpriK and NpriL
+            !                --->             --->             --->            --->
+            ! ->     expo(I)*xyz(I) + expo(J)*xyz(J) + expo(K)*xyz(K) +expo(L)*xyz(L)
+            ! W = -------------------------------------------------------------------
+            !                    expo(I) + expo(J) + expo(K) + expo(L)
+            W(M)=(AAtemp(M)+Q(M)*CD)/ABCD
+
+            !Required for HGP Eqn 13.
+            !        ->  ->  2
+            ! RPQ =| P - Q |
+            XXXtemp=P(M)-Q(M)
+            RPQ=RPQ+XXXtemp*XXXtemp            
+
+            ! ----->   ->  ->
+            ! WQtemp = W - Q
+            ! ----->   ->  ->
+            ! WPtemp = W - P
+            WQtemp(M)=W(M)-Q(M)
+
+            !Required for HGP Eqns 6 and 16.
+            WPtemp(M)=W(M)-P(M)
+
+          enddo
+
+          !HGP Eqn 13. 
+          !             ->  -> 2
+          ! T = ROU * | P - Q|
+          T=RPQ*ROU
+
+          ! Compute boys function values, HGP eqn 14.
+          !                         2m        2
+          ! Fm(T) = integral(1,0) {t   exp(-Tt )dt}
+          ! NABCD is the m value, and FM returns the FmT value
+#ifdef MIRP
+          call mirp_fmt(NABCD,T,FM)
+#else
+          call FmT(NABCD,T,FM)
+#endif
+
+          !Go through all m values, obtain Fm values from FM array we
+          !just computed and calculate quantities required for HGP Eqn
+          !12. 
+          do iitemp=0,NABCD
+             ! Yxiaotemp(1,1,iitemp) is the starting point of recurrsion
+             Yxiaotemp(1,1,iitemp)=FM(iitemp)/ABCDsqrt
+          enddo
+
+          ITT=ITT+1
+
+          ! now we will do vrr and and the double-electron integral
+          call vertical(NABCDTYPE)
+
+          do I2=NNC,NNCD
+             do I1=NNA,NNAB
+                Yxiao(ITT,I1,I2)=Yxiaotemp(I1,I2,0)
+             enddo
+          enddo          
+
+        !endif
+      enddo
+    enddo
 
   end subroutine compute_lngr_int
 
