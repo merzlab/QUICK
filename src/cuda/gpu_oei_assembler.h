95194bb553cbd98ccbddca375efe83a6c9b5603c
Madu86 <mmmskumara@gmail.com>
2021-07-14 08:39:54 -0700

    fixed bugs

--- src/cuda/gpu_oei_assembler.h
+++ src/cuda/gpu_oei_assembler.h
@@ -1,6 +1,6 @@
 /*
  !---------------------------------------------------------------------!
- ! Written by QUICK-GenInt code generator on 10/07/2021                !
+ ! Written by QUICK-GenInt code generator on 14/07/2021                !
  !                                                                     !
  ! Copyright (C) 2020-2021 Merz lab                                    !
  ! Copyright (C) 2020-2021 Götz lab                                    !
@@ -11,176 +11,152 @@
  !_____________________________________________________________________!
 */
 
-__device__ __inline__ void OEint_vertical(int I, int J, QUICKDouble PAx, QUICKDouble PAy, QUICKDouble PAz,
+__device__ __inline__ void OEint_vertical(int I, int J, int II, int JJ,QUICKDouble PAx, QUICKDouble PAy, QUICKDouble PAz,
         QUICKDouble PBx, QUICKDouble PBy, QUICKDouble PBz, QUICKDouble PCx, QUICKDouble PCy, QUICKDouble PCz, QUICKDouble Zeta,
         QUICKDouble* store, QUICKDouble* YVerticalTemp){ 
 
   /* SS integral, m=0 */ 
   if(I == 0 && J == 0){ 
-    LOC2(store, 0, 0, STOREDIM, STOREDIM) = VY(0, 0, 0);
-
-    printf("II %d JJ %d store SS: %f \n", I, J, LOC2(store, 0, 0, STOREDIM, STOREDIM));
-
+    LOC2(store, 0, 0, STOREDIM, STOREDIM) += VY(0, 0, 0);
   } 
 
   /* PS integral, m=0 */ 
   if(I == 1 && J == 0){ 
     PSint_0 ps(PAx, PAy, PAz, PCx, PCy, PCz, YVerticalTemp); 
-    LOC2(store, 1, 0, STOREDIM, STOREDIM) = ps.x_1_0;
-    LOC2(store, 2, 0, STOREDIM, STOREDIM) = ps.x_2_0;
-    LOC2(store, 3, 0, STOREDIM, STOREDIM) = ps.x_3_0;
-
-    printf("II %d JJ %d store PS: x_1_0 %f \n", I, J, LOC2(store, 1, 0, STOREDIM, STOREDIM));
-    printf("II %d JJ %d store PS: x_2_0 %f \n", I, J, LOC2(store, 2, 0, STOREDIM, STOREDIM));
-    printf("II %d JJ %d store PS: x_3_0 %f \n", I, J, LOC2(store, 3, 0, STOREDIM, STOREDIM));
-
+    LOC2(store, 1, 0, STOREDIM, STOREDIM) += ps.x_1_0;
+    LOC2(store, 2, 0, STOREDIM, STOREDIM) += ps.x_2_0;
+    LOC2(store, 3, 0, STOREDIM, STOREDIM) += ps.x_3_0;
   } 
 
   /* SP integral, m=0 */ 
   if(I == 0 && J == 1){ 
     SPint_0 sp(PBx, PBy, PBz, PCx, PCy, PCz, YVerticalTemp); 
-    LOC2(store, 0, 1, STOREDIM, STOREDIM) = sp.x_0_1;
-    LOC2(store, 0, 2, STOREDIM, STOREDIM) = sp.x_0_2;
-    LOC2(store, 0, 3, STOREDIM, STOREDIM) = sp.x_0_3;
-
-    printf("II %d JJ %d store SP: x_0_1 %f \n", I, J, LOC2(store, 0, 1, STOREDIM, STOREDIM));
-    printf("II %d JJ %d store SP: x_0_2 %f \n", I, J, LOC2(store, 0, 2, STOREDIM, STOREDIM));
-    printf("II %d JJ %d store SP: x_0_3 %f \n", I, J, LOC2(store, 0, 3, STOREDIM, STOREDIM));
-
+    LOC2(store, 0, 1, STOREDIM, STOREDIM) += sp.x_0_1;
+    LOC2(store, 0, 2, STOREDIM, STOREDIM) += sp.x_0_2;
+    LOC2(store, 0, 3, STOREDIM, STOREDIM) += sp.x_0_3;
   } 
 
   /* PP integral, m=0 */ 
   if(I == 1 && J == 1){ 
     PPint_0 pp(PAx, PAy, PAz, PBx, PBy, PBz, PCx, PCy, PCz, Zeta, YVerticalTemp); 
-    LOC2(store, 1, 1, STOREDIM, STOREDIM) = pp.x_1_1;
-    LOC2(store, 1, 2, STOREDIM, STOREDIM) = pp.x_1_2;
-    LOC2(store, 1, 3, STOREDIM, STOREDIM) = pp.x_1_3;
-    LOC2(store, 2, 1, STOREDIM, STOREDIM) = pp.x_2_1;
-    LOC2(store, 2, 2, STOREDIM, STOREDIM) = pp.x_2_2;
-    LOC2(store, 2, 3, STOREDIM, STOREDIM) = pp.x_2_3;
-    LOC2(store, 3, 1, STOREDIM, STOREDIM) = pp.x_3_1;
-    LOC2(store, 3, 2, STOREDIM, STOREDIM) = pp.x_3_2;
-    LOC2(store, 3, 3, STOREDIM, STOREDIM) = pp.x_3_3;
-
-    printf("II %d JJ %d store PP: x_1_1 %f \n", I, J, LOC2(store, 1, 1, STOREDIM, STOREDIM));
-    printf("II %d JJ %d store PP: x_1_2 %f \n", I, J, LOC2(store, 1, 2, STOREDIM, STOREDIM));
-    printf("II %d JJ %d store PP: x_1_3 %f \n", I, J, LOC2(store, 1, 3, STOREDIM, STOREDIM));
-    printf("II %d JJ %d store PP: x_2_1 %f \n", I, J, LOC2(store, 2, 1, STOREDIM, STOREDIM));
-    printf("II %d JJ %d store PP: x_2_2 %f \n", I, J, LOC2(store, 2, 2, STOREDIM, STOREDIM));
-    printf("II %d JJ %d store PP: x_2_3 %f \n", I, J, LOC2(store, 2, 3, STOREDIM, STOREDIM));
-    printf("II %d JJ %d store PP: x_3_1 %f \n", I, J, LOC2(store, 3, 1, STOREDIM, STOREDIM));
-    printf("II %d JJ %d store PP: x_3_2 %f \n", I, J, LOC2(store, 3, 2, STOREDIM, STOREDIM));
-    printf("II %d JJ %d store PP: x_3_3 %f \n", I, J, LOC2(store, 3, 3, STOREDIM, STOREDIM));
-
+    LOC2(store, 1, 1, STOREDIM, STOREDIM) += pp.x_1_1;
+    LOC2(store, 1, 2, STOREDIM, STOREDIM) += pp.x_1_2;
+    LOC2(store, 1, 3, STOREDIM, STOREDIM) += pp.x_1_3;
+    LOC2(store, 2, 1, STOREDIM, STOREDIM) += pp.x_2_1;
+    LOC2(store, 2, 2, STOREDIM, STOREDIM) += pp.x_2_2;
+    LOC2(store, 2, 3, STOREDIM, STOREDIM) += pp.x_2_3;
+    LOC2(store, 3, 1, STOREDIM, STOREDIM) += pp.x_3_1;
+    LOC2(store, 3, 2, STOREDIM, STOREDIM) += pp.x_3_2;
+    LOC2(store, 3, 3, STOREDIM, STOREDIM) += pp.x_3_3;
   } 
 
   /* DS integral, m=0 */ 
   if(I == 2 && J == 0){ 
     DSint_0 ds(PAx, PAy, PAz, PCx, PCy, PCz, Zeta, YVerticalTemp); 
-    LOC2(store, 4, 0, STOREDIM, STOREDIM) = ds.x_4_0;
-    LOC2(store, 5, 0, STOREDIM, STOREDIM) = ds.x_5_0;
-    LOC2(store, 6, 0, STOREDIM, STOREDIM) = ds.x_6_0;
-    LOC2(store, 7, 0, STOREDIM, STOREDIM) = ds.x_7_0;
-    LOC2(store, 8, 0, STOREDIM, STOREDIM) = ds.x_8_0;
-    LOC2(store, 9, 0, STOREDIM, STOREDIM) = ds.x_9_0;
+    LOC2(store, 4, 0, STOREDIM, STOREDIM) += ds.x_4_0;
+    LOC2(store, 5, 0, STOREDIM, STOREDIM) += ds.x_5_0;
+    LOC2(store, 6, 0, STOREDIM, STOREDIM) += ds.x_6_0;
+    LOC2(store, 7, 0, STOREDIM, STOREDIM) += ds.x_7_0;
+    LOC2(store, 8, 0, STOREDIM, STOREDIM) += ds.x_8_0;
+    LOC2(store, 9, 0, STOREDIM, STOREDIM) += ds.x_9_0;
   } 
 
   /* SD integral, m=0 */ 
   if(I == 0 && J == 2){ 
     SDint_0 sd(PAx, PAy, PAz, PCx, PCy, PCz, Zeta, YVerticalTemp); 
-    LOC2(store, 0, 4, STOREDIM, STOREDIM) = sd.x_0_4;
-    LOC2(store, 0, 5, STOREDIM, STOREDIM) = sd.x_0_5;
-    LOC2(store, 0, 6, STOREDIM, STOREDIM) = sd.x_0_6;
-    LOC2(store, 0, 7, STOREDIM, STOREDIM) = sd.x_0_7;
-    LOC2(store, 0, 8, STOREDIM, STOREDIM) = sd.x_0_8;
-    LOC2(store, 0, 9, STOREDIM, STOREDIM) = sd.x_0_9;
+    LOC2(store, 0, 4, STOREDIM, STOREDIM) += sd.x_0_4;
+    LOC2(store, 0, 5, STOREDIM, STOREDIM) += sd.x_0_5;
+    LOC2(store, 0, 6, STOREDIM, STOREDIM) += sd.x_0_6;
+    LOC2(store, 0, 7, STOREDIM, STOREDIM) += sd.x_0_7;
+    LOC2(store, 0, 8, STOREDIM, STOREDIM) += sd.x_0_8;
+    LOC2(store, 0, 9, STOREDIM, STOREDIM) += sd.x_0_9;
   } 
 
   /* DP integral, m=0 */ 
   if(I == 2 && J == 1){ 
     DPint_0 dp(PAx, PAy, PAz, PBx, PBy, PBz, PCx, PCy, PCz, Zeta, YVerticalTemp); 
-    LOC2(store, 4, 1, STOREDIM, STOREDIM) = dp.x_4_1;
-    LOC2(store, 4, 2, STOREDIM, STOREDIM) = dp.x_4_2;
-    LOC2(store, 4, 3, STOREDIM, STOREDIM) = dp.x_4_3;
-    LOC2(store, 5, 1, STOREDIM, STOREDIM) = dp.x_5_1;
-    LOC2(store, 5, 2, STOREDIM, STOREDIM) = dp.x_5_2;
-    LOC2(store, 5, 3, STOREDIM, STOREDIM) = dp.x_5_3;
-    LOC2(store, 6, 1, STOREDIM, STOREDIM) = dp.x_6_1;
-    LOC2(store, 6, 2, STOREDIM, STOREDIM) = dp.x_6_2;
-    LOC2(store, 6, 3, STOREDIM, STOREDIM) = dp.x_6_3;
-    LOC2(store, 7, 1, STOREDIM, STOREDIM) = dp.x_7_1;
-    LOC2(store, 7, 2, STOREDIM, STOREDIM) = dp.x_7_2;
-    LOC2(store, 7, 3, STOREDIM, STOREDIM) = dp.x_7_3;
-    LOC2(store, 8, 1, STOREDIM, STOREDIM) = dp.x_8_1;
-    LOC2(store, 8, 2, STOREDIM, STOREDIM) = dp.x_8_2;
-    LOC2(store, 8, 3, STOREDIM, STOREDIM) = dp.x_8_3;
-    LOC2(store, 9, 1, STOREDIM, STOREDIM) = dp.x_9_1;
-    LOC2(store, 9, 2, STOREDIM, STOREDIM) = dp.x_9_2;
-    LOC2(store, 9, 3, STOREDIM, STOREDIM) = dp.x_9_3;
+    LOC2(store, 4, 1, STOREDIM, STOREDIM) += dp.x_4_1;
+    LOC2(store, 4, 2, STOREDIM, STOREDIM) += dp.x_4_2;
+    LOC2(store, 4, 3, STOREDIM, STOREDIM) += dp.x_4_3;
+    LOC2(store, 5, 1, STOREDIM, STOREDIM) += dp.x_5_1;
+    LOC2(store, 5, 2, STOREDIM, STOREDIM) += dp.x_5_2;
+    LOC2(store, 5, 3, STOREDIM, STOREDIM) += dp.x_5_3;
+    LOC2(store, 6, 1, STOREDIM, STOREDIM) += dp.x_6_1;
+    LOC2(store, 6, 2, STOREDIM, STOREDIM) += dp.x_6_2;
+    LOC2(store, 6, 3, STOREDIM, STOREDIM) += dp.x_6_3;
+    LOC2(store, 7, 1, STOREDIM, STOREDIM) += dp.x_7_1;
+    LOC2(store, 7, 2, STOREDIM, STOREDIM) += dp.x_7_2;
+    LOC2(store, 7, 3, STOREDIM, STOREDIM) += dp.x_7_3;
+    LOC2(store, 8, 1, STOREDIM, STOREDIM) += dp.x_8_1;
+    LOC2(store, 8, 2, STOREDIM, STOREDIM) += dp.x_8_2;
+    LOC2(store, 8, 3, STOREDIM, STOREDIM) += dp.x_8_3;
+    LOC2(store, 9, 1, STOREDIM, STOREDIM) += dp.x_9_1;
+    LOC2(store, 9, 2, STOREDIM, STOREDIM) += dp.x_9_2;
+    LOC2(store, 9, 3, STOREDIM, STOREDIM) += dp.x_9_3;
   } 
 
   /* PD integral, m=0 */ 
   if(I == 1 && J == 2){ 
     PDint_0 pd(PAx, PAy, PAz, PBx, PBy, PBz, PCx, PCy, PCz, Zeta, YVerticalTemp); 
-    LOC2(store, 1, 4, STOREDIM, STOREDIM) = pd.x_1_4;
-    LOC2(store, 2, 4, STOREDIM, STOREDIM) = pd.x_2_4;
-    LOC2(store, 3, 4, STOREDIM, STOREDIM) = pd.x_3_4;
-    LOC2(store, 1, 5, STOREDIM, STOREDIM) = pd.x_1_5;
-    LOC2(store, 2, 5, STOREDIM, STOREDIM) = pd.x_2_5;
-    LOC2(store, 3, 5, STOREDIM, STOREDIM) = pd.x_3_5;
-    LOC2(store, 1, 6, STOREDIM, STOREDIM) = pd.x_1_6;
-    LOC2(store, 2, 6, STOREDIM, STOREDIM) = pd.x_2_6;
-    LOC2(store, 3, 6, STOREDIM, STOREDIM) = pd.x_3_6;
-    LOC2(store, 1, 7, STOREDIM, STOREDIM) = pd.x_1_7;
-    LOC2(store, 2, 7, STOREDIM, STOREDIM) = pd.x_2_7;
-    LOC2(store, 3, 7, STOREDIM, STOREDIM) = pd.x_3_7;
-    LOC2(store, 1, 8, STOREDIM, STOREDIM) = pd.x_1_8;
-    LOC2(store, 2, 8, STOREDIM, STOREDIM) = pd.x_2_8;
-    LOC2(store, 3, 8, STOREDIM, STOREDIM) = pd.x_3_8;
-    LOC2(store, 1, 9, STOREDIM, STOREDIM) = pd.x_1_9;
-    LOC2(store, 2, 9, STOREDIM, STOREDIM) = pd.x_2_9;
-    LOC2(store, 3, 9, STOREDIM, STOREDIM) = pd.x_3_9;
+    LOC2(store, 1, 4, STOREDIM, STOREDIM) += pd.x_1_4;
+    LOC2(store, 2, 4, STOREDIM, STOREDIM) += pd.x_2_4;
+    LOC2(store, 3, 4, STOREDIM, STOREDIM) += pd.x_3_4;
+    LOC2(store, 1, 5, STOREDIM, STOREDIM) += pd.x_1_5;
+    LOC2(store, 2, 5, STOREDIM, STOREDIM) += pd.x_2_5;
+    LOC2(store, 3, 5, STOREDIM, STOREDIM) += pd.x_3_5;
+    LOC2(store, 1, 6, STOREDIM, STOREDIM) += pd.x_1_6;
+    LOC2(store, 2, 6, STOREDIM, STOREDIM) += pd.x_2_6;
+    LOC2(store, 3, 6, STOREDIM, STOREDIM) += pd.x_3_6;
+    LOC2(store, 1, 7, STOREDIM, STOREDIM) += pd.x_1_7;
+    LOC2(store, 2, 7, STOREDIM, STOREDIM) += pd.x_2_7;
+    LOC2(store, 3, 7, STOREDIM, STOREDIM) += pd.x_3_7;
+    LOC2(store, 1, 8, STOREDIM, STOREDIM) += pd.x_1_8;
+    LOC2(store, 2, 8, STOREDIM, STOREDIM) += pd.x_2_8;
+    LOC2(store, 3, 8, STOREDIM, STOREDIM) += pd.x_3_8;
+    LOC2(store, 1, 9, STOREDIM, STOREDIM) += pd.x_1_9;
+    LOC2(store, 2, 9, STOREDIM, STOREDIM) += pd.x_2_9;
+    LOC2(store, 3, 9, STOREDIM, STOREDIM) += pd.x_3_9;
   } 
 
   /* DD integral, m=0 */ 
   if(I == 2 && J == 2){ 
     DDint_0 dd(PAx, PAy, PAz, PBx, PBy, PBz, PCx, PCy, PCz, Zeta, YVerticalTemp); 
-    LOC2(store, 4, 4, STOREDIM, STOREDIM) = dd.x_4_4;
-    LOC2(store, 5, 4, STOREDIM, STOREDIM) = dd.x_5_4;
-    LOC2(store, 6, 4, STOREDIM, STOREDIM) = dd.x_6_4;
-    LOC2(store, 7, 4, STOREDIM, STOREDIM) = dd.x_7_4;
-    LOC2(store, 8, 4, STOREDIM, STOREDIM) = dd.x_8_4;
-    LOC2(store, 9, 4, STOREDIM, STOREDIM) = dd.x_9_4;
-    LOC2(store, 4, 5, STOREDIM, STOREDIM) = dd.x_4_5;
-    LOC2(store, 5, 5, STOREDIM, STOREDIM) = dd.x_5_5;
-    LOC2(store, 6, 5, STOREDIM, STOREDIM) = dd.x_6_5;
-    LOC2(store, 7, 5, STOREDIM, STOREDIM) = dd.x_7_5;
-    LOC2(store, 8, 5, STOREDIM, STOREDIM) = dd.x_8_5;
-    LOC2(store, 9, 5, STOREDIM, STOREDIM) = dd.x_9_5;
-    LOC2(store, 4, 6, STOREDIM, STOREDIM) = dd.x_4_6;
-    LOC2(store, 5, 6, STOREDIM, STOREDIM) = dd.x_5_6;
-    LOC2(store, 6, 6, STOREDIM, STOREDIM) = dd.x_6_6;
-    LOC2(store, 7, 6, STOREDIM, STOREDIM) = dd.x_7_6;
-    LOC2(store, 8, 6, STOREDIM, STOREDIM) = dd.x_8_6;
-    LOC2(store, 9, 6, STOREDIM, STOREDIM) = dd.x_9_6;
-    LOC2(store, 4, 7, STOREDIM, STOREDIM) = dd.x_4_7;
-    LOC2(store, 5, 7, STOREDIM, STOREDIM) = dd.x_5_7;
-    LOC2(store, 6, 7, STOREDIM, STOREDIM) = dd.x_6_7;
-    LOC2(store, 7, 7, STOREDIM, STOREDIM) = dd.x_7_7;
-    LOC2(store, 8, 7, STOREDIM, STOREDIM) = dd.x_8_7;
-    LOC2(store, 9, 7, STOREDIM, STOREDIM) = dd.x_9_7;
-    LOC2(store, 4, 8, STOREDIM, STOREDIM) = dd.x_4_8;
-    LOC2(store, 5, 8, STOREDIM, STOREDIM) = dd.x_5_8;
-    LOC2(store, 6, 8, STOREDIM, STOREDIM) = dd.x_6_8;
-    LOC2(store, 7, 8, STOREDIM, STOREDIM) = dd.x_7_8;
-    LOC2(store, 8, 8, STOREDIM, STOREDIM) = dd.x_8_8;
-    LOC2(store, 9, 8, STOREDIM, STOREDIM) = dd.x_9_8;
-    LOC2(store, 4, 9, STOREDIM, STOREDIM) = dd.x_4_9;
-    LOC2(store, 5, 9, STOREDIM, STOREDIM) = dd.x_5_9;
-    LOC2(store, 6, 9, STOREDIM, STOREDIM) = dd.x_6_9;
-    LOC2(store, 7, 9, STOREDIM, STOREDIM) = dd.x_7_9;
-    LOC2(store, 8, 9, STOREDIM, STOREDIM) = dd.x_8_9;
-    LOC2(store, 9, 9, STOREDIM, STOREDIM) = dd.x_9_9;
+    LOC2(store, 4, 4, STOREDIM, STOREDIM) += dd.x_4_4;
+    LOC2(store, 5, 4, STOREDIM, STOREDIM) += dd.x_5_4;
+    LOC2(store, 6, 4, STOREDIM, STOREDIM) += dd.x_6_4;
+    LOC2(store, 7, 4, STOREDIM, STOREDIM) += dd.x_7_4;
+    LOC2(store, 8, 4, STOREDIM, STOREDIM) += dd.x_8_4;
+    LOC2(store, 9, 4, STOREDIM, STOREDIM) += dd.x_9_4;
+    LOC2(store, 4, 5, STOREDIM, STOREDIM) += dd.x_4_5;
+    LOC2(store, 5, 5, STOREDIM, STOREDIM) += dd.x_5_5;
+    LOC2(store, 6, 5, STOREDIM, STOREDIM) += dd.x_6_5;
+    LOC2(store, 7, 5, STOREDIM, STOREDIM) += dd.x_7_5;
+    LOC2(store, 8, 5, STOREDIM, STOREDIM) += dd.x_8_5;
+    LOC2(store, 9, 5, STOREDIM, STOREDIM) += dd.x_9_5;
+    LOC2(store, 4, 6, STOREDIM, STOREDIM) += dd.x_4_6;
+    LOC2(store, 5, 6, STOREDIM, STOREDIM) += dd.x_5_6;
+    LOC2(store, 6, 6, STOREDIM, STOREDIM) += dd.x_6_6;
+    LOC2(store, 7, 6, STOREDIM, STOREDIM) += dd.x_7_6;
+    LOC2(store, 8, 6, STOREDIM, STOREDIM) += dd.x_8_6;
+    LOC2(store, 9, 6, STOREDIM, STOREDIM) += dd.x_9_6;
+    LOC2(store, 4, 7, STOREDIM, STOREDIM) += dd.x_4_7;
+    LOC2(store, 5, 7, STOREDIM, STOREDIM) += dd.x_5_7;
+    LOC2(store, 6, 7, STOREDIM, STOREDIM) += dd.x_6_7;
+    LOC2(store, 7, 7, STOREDIM, STOREDIM) += dd.x_7_7;
+    LOC2(store, 8, 7, STOREDIM, STOREDIM) += dd.x_8_7;
+    LOC2(store, 9, 7, STOREDIM, STOREDIM) += dd.x_9_7;
+    LOC2(store, 4, 8, STOREDIM, STOREDIM) += dd.x_4_8;
+    LOC2(store, 5, 8, STOREDIM, STOREDIM) += dd.x_5_8;
+    LOC2(store, 6, 8, STOREDIM, STOREDIM) += dd.x_6_8;
+    LOC2(store, 7, 8, STOREDIM, STOREDIM) += dd.x_7_8;
+    LOC2(store, 8, 8, STOREDIM, STOREDIM) += dd.x_8_8;
+    LOC2(store, 9, 8, STOREDIM, STOREDIM) += dd.x_9_8;
+    LOC2(store, 4, 9, STOREDIM, STOREDIM) += dd.x_4_9;
+    LOC2(store, 5, 9, STOREDIM, STOREDIM) += dd.x_5_9;
+    LOC2(store, 6, 9, STOREDIM, STOREDIM) += dd.x_6_9;
+    LOC2(store, 7, 9, STOREDIM, STOREDIM) += dd.x_7_9;
+    LOC2(store, 8, 9, STOREDIM, STOREDIM) += dd.x_8_9;
+    LOC2(store, 9, 9, STOREDIM, STOREDIM) += dd.x_9_9;
   } 
 
  } 
--- src/cuda/gpu_oei_definitions.h
+++ src/cuda/gpu_oei_definitions.h
@@ -1,6 +1,6 @@
 /*
  !---------------------------------------------------------------------!
- ! Written by QUICK-GenInt code generator on 10/07/2021                !
+ ! Written by QUICK-GenInt code generator on 14/07/2021                !
  !                                                                     !
  ! Copyright (C) 2020-2021 Merz lab                                    !
  ! Copyright (C) 2020-2021 Götz lab                                    !
@@ -16,9 +16,9 @@
 __device__ __inline__ PSint_0::PSint_0(QUICKDouble PAx, QUICKDouble PAy, QUICKDouble PAz,
                 QUICKDouble PCx, QUICKDouble PCy, QUICKDouble PCz, QUICKDouble* YVerticalTemp){ 
 
-  x_1_0 = PAx * VY(0, 0, 0) - PCx * VY(0, 0, 0);
-  x_2_0 = PAy * VY(0, 0, 0) - PCy * VY(0, 0, 0);
-  x_3_0 = PAz * VY(0, 0, 0) - PCz * VY(0, 0, 0);
+  x_1_0 = PAx * VY(0, 0, 0) - PCx * VY(0, 0, 1);
+  x_2_0 = PAy * VY(0, 0, 0) - PCy * VY(0, 0, 1);
+  x_3_0 = PAz * VY(0, 0, 0) - PCz * VY(0, 0, 1);
 } 
 
 
@@ -26,9 +26,9 @@ __device__ __inline__ PSint_0::PSint_0(QUICKDouble PAx, QUICKDouble PAy, QUICKDo
 __device__ __inline__ PSint_1::PSint_1(QUICKDouble PAx, QUICKDouble PAy, QUICKDouble PAz,
                 QUICKDouble PCx, QUICKDouble PCy, QUICKDouble PCz, QUICKDouble* YVerticalTemp){ 
 
-  x_1_0 = PAx * VY(0, 0, 1) - PCx * VY(0, 0, 1);
-  x_2_0 = PAy * VY(0, 0, 1) - PCy * VY(0, 0, 1);
-  x_3_0 = PAz * VY(0, 0, 1) - PCz * VY(0, 0, 1);
+  x_1_0 = PAx * VY(0, 0, 1) - PCx * VY(0, 0, 2);
+  x_2_0 = PAy * VY(0, 0, 1) - PCy * VY(0, 0, 2);
+  x_3_0 = PAz * VY(0, 0, 1) - PCz * VY(0, 0, 2);
 } 
 
 
@@ -36,9 +36,9 @@ __device__ __inline__ PSint_1::PSint_1(QUICKDouble PAx, QUICKDouble PAy, QUICKDo
 __device__ __inline__ PSint_2::PSint_2(QUICKDouble PAx, QUICKDouble PAy, QUICKDouble PAz,
                 QUICKDouble PCx, QUICKDouble PCy, QUICKDouble PCz, QUICKDouble* YVerticalTemp){ 
 
-  x_1_0 = PAx * VY(0, 0, 2) - PCx * VY(0, 0, 2);
-  x_2_0 = PAy * VY(0, 0, 2) - PCy * VY(0, 0, 2);
-  x_3_0 = PAz * VY(0, 0, 2) - PCz * VY(0, 0, 2);
+  x_1_0 = PAx * VY(0, 0, 2) - PCx * VY(0, 0, 3);
+  x_2_0 = PAy * VY(0, 0, 2) - PCy * VY(0, 0, 3);
+  x_3_0 = PAz * VY(0, 0, 2) - PCz * VY(0, 0, 3);
 } 
 
 
@@ -46,9 +46,9 @@ __device__ __inline__ PSint_2::PSint_2(QUICKDouble PAx, QUICKDouble PAy, QUICKDo
 __device__ __inline__ PSint_3::PSint_3(QUICKDouble PAx, QUICKDouble PAy, QUICKDouble PAz,
                 QUICKDouble PCx, QUICKDouble PCy, QUICKDouble PCz, QUICKDouble* YVerticalTemp){ 
 
-  x_1_0 = PAx * VY(0, 0, 3) - PCx * VY(0, 0, 3);
-  x_2_0 = PAy * VY(0, 0, 3) - PCy * VY(0, 0, 3);
-  x_3_0 = PAz * VY(0, 0, 3) - PCz * VY(0, 0, 3);
+  x_1_0 = PAx * VY(0, 0, 3) - PCx * VY(0, 0, 4);
+  x_2_0 = PAy * VY(0, 0, 3) - PCy * VY(0, 0, 4);
+  x_3_0 = PAz * VY(0, 0, 3) - PCz * VY(0, 0, 4);
 } 
 
 
@@ -56,9 +56,9 @@ __device__ __inline__ PSint_3::PSint_3(QUICKDouble PAx, QUICKDouble PAy, QUICKDo
 __device__ __inline__ SPint_0::SPint_0(QUICKDouble PBx, QUICKDouble PBy, QUICKDouble PBz,
                 QUICKDouble PCx, QUICKDouble PCy, QUICKDouble PCz, QUICKDouble* YVerticalTemp){ 
 
-  x_0_1 = PBx * VY(0, 0, 0) - PCx * VY(0, 0, 0);
-  x_0_2 = PBy * VY(0, 0, 0) - PCy * VY(0, 0, 0);
-  x_0_3 = PBz * VY(0, 0, 0) - PCz * VY(0, 0, 0);
+  x_0_1 = PBx * VY(0, 0, 0) - PCx * VY(0, 0, 1);
+  x_0_2 = PBy * VY(0, 0, 0) - PCy * VY(0, 0, 1);
+  x_0_3 = PBz * VY(0, 0, 0) - PCz * VY(0, 0, 1);
 } 
 
 
@@ -66,9 +66,9 @@ __device__ __inline__ SPint_0::SPint_0(QUICKDouble PBx, QUICKDouble PBy, QUICKDo
 __device__ __inline__ SPint_1::SPint_1(QUICKDouble PBx, QUICKDouble PBy, QUICKDouble PBz,
                 QUICKDouble PCx, QUICKDouble PCy, QUICKDouble PCz, QUICKDouble* YVerticalTemp){ 
 
-  x_0_1 = PBx * VY(0, 0, 1) - PCx * VY(0, 0, 1);
-  x_0_2 = PBy * VY(0, 0, 1) - PCy * VY(0, 0, 1);
-  x_0_3 = PBz * VY(0, 0, 1) - PCz * VY(0, 0, 1);
+  x_0_1 = PBx * VY(0, 0, 1) - PCx * VY(0, 0, 2);
+  x_0_2 = PBy * VY(0, 0, 1) - PCy * VY(0, 0, 2);
+  x_0_3 = PBz * VY(0, 0, 1) - PCz * VY(0, 0, 2);
 } 
 
 
@@ -76,9 +76,9 @@ __device__ __inline__ SPint_1::SPint_1(QUICKDouble PBx, QUICKDouble PBy, QUICKDo
 __device__ __inline__ SPint_2::SPint_2(QUICKDouble PBx, QUICKDouble PBy, QUICKDouble PBz,
                 QUICKDouble PCx, QUICKDouble PCy, QUICKDouble PCz, QUICKDouble* YVerticalTemp){ 
 
-  x_0_1 = PBx * VY(0, 0, 2) - PCx * VY(0, 0, 2);
-  x_0_2 = PBy * VY(0, 0, 2) - PCy * VY(0, 0, 2);
-  x_0_3 = PBz * VY(0, 0, 2) - PCz * VY(0, 0, 2);
+  x_0_1 = PBx * VY(0, 0, 2) - PCx * VY(0, 0, 3);
+  x_0_2 = PBy * VY(0, 0, 2) - PCy * VY(0, 0, 3);
+  x_0_3 = PBz * VY(0, 0, 2) - PCz * VY(0, 0, 3);
 } 
 
 
@@ -86,9 +86,9 @@ __device__ __inline__ SPint_2::SPint_2(QUICKDouble PBx, QUICKDouble PBy, QUICKDo
 __device__ __inline__ SPint_3::SPint_3(QUICKDouble PBx, QUICKDouble PBy, QUICKDouble PBz,
                 QUICKDouble PCx, QUICKDouble PCy, QUICKDouble PCz, QUICKDouble* YVerticalTemp){ 
 
-  x_0_1 = PBx * VY(0, 0, 3) - PCx * VY(0, 0, 3);
-  x_0_2 = PBy * VY(0, 0, 3) - PCy * VY(0, 0, 3);
-  x_0_3 = PBz * VY(0, 0, 3) - PCz * VY(0, 0, 3);
+  x_0_1 = PBx * VY(0, 0, 3) - PCx * VY(0, 0, 4);
+  x_0_2 = PBy * VY(0, 0, 3) - PCy * VY(0, 0, 4);
+  x_0_3 = PBz * VY(0, 0, 3) - PCz * VY(0, 0, 4);
 } 
 
 
@@ -339,8 +339,8 @@ __device__ __inline__ PDint_0::PDint_0(QUICKDouble PAx, QUICKDouble PAy, QUICKDo
 
   SPint_0 sp_0(PBx, PBy, PBz, PCx, PCy, PCz, YVerticalTemp); // construct [s|p] for m=0 
   SPint_1 sp_1(PBx, PBy, PBz, PCx, PCy, PCz, YVerticalTemp); // construct [s|p] for m=1 
-  SDint_0 sd_0(PAx, PAy, PAz, PCx, PCy, PCz, Zeta, YVerticalTemp); // construct [s|d] for m=0 
-  SDint_1 sd_1(PAx, PAy, PAz, PCx, PCy, PCz, Zeta, YVerticalTemp); // construct [s|d] for m=1 
+  SDint_0 sd_0(PBx, PBy, PBz, PCx, PCy, PCz, Zeta, YVerticalTemp); // construct [s|d] for m=0 
+  SDint_1 sd_1(PBx, PBy, PBz, PCx, PCy, PCz, Zeta, YVerticalTemp); // construct [s|d] for m=1 
 
   x_1_4 = PAx * sd_0.x_0_4 - PCx * sd_1.x_0_4; 
   x_1_4 += 0.5/Zeta * 1.000000 * (sp_0.x_0_2 - sp_1.x_0_2); 
@@ -385,94 +385,76 @@ __device__ __inline__ DDint_0::DDint_0(QUICKDouble PAx, QUICKDouble PAy, QUICKDo
   DPint_1 dp_1(PAx, PAy, PAz, PBx, PBy, PBz, PCx, PCy, PCz, Zeta, YVerticalTemp); // construct [d|p] for m=1 
 
   x_4_4 = PBx * dp_0.x_4_2 - PCx * dp_1.x_4_2; 
-  x_4_4 += 0.5/Zeta * 1.000000 * (ds_0.x_4_0 - ds_1.x_4_0); 
   x_4_4 += 0.5/Zeta * 1.000000 * (pp_0.x_2_2 - pp_1.x_2_2); 
   x_5_4 = PBy * dp_0.x_4_3 - PCy * dp_1.x_4_3; 
-  x_5_4 += 0.5/Zeta * 1.000000 * (ds_0.x_4_0 - ds_1.x_4_0); 
   x_5_4 += 0.5/Zeta * 1.000000 * (pp_0.x_1_3 - pp_1.x_1_3); 
   x_6_4 = PBx * dp_0.x_4_3 - PCx * dp_1.x_4_3; 
-  x_6_4 += 0.5/Zeta * 1.000000 * (ds_0.x_4_0 - ds_1.x_4_0); 
   x_6_4 += 0.5/Zeta * 1.000000 * (pp_0.x_2_3 - pp_1.x_2_3); 
   x_7_4 = PBx * dp_0.x_4_1 - PCx * dp_1.x_4_1; 
-  x_7_4 += 0.5/Zeta * 2.000000 * (ds_0.x_4_0 - ds_1.x_4_0); 
+  x_7_4 += 0.5/Zeta * 1.000000 * (ds_0.x_4_0 - ds_1.x_4_0); 
   x_7_4 += 0.5/Zeta * 1.000000 * (pp_0.x_2_1 - pp_1.x_2_1); 
   x_8_4 = PBy * dp_0.x_4_2 - PCy * dp_1.x_4_2; 
-  x_8_4 += 0.5/Zeta * 2.000000 * (ds_0.x_4_0 - ds_1.x_4_0); 
+  x_8_4 += 0.5/Zeta * 1.000000 * (ds_0.x_4_0 - ds_1.x_4_0); 
   x_8_4 += 0.5/Zeta * 1.000000 * (pp_0.x_1_2 - pp_1.x_1_2); 
   x_9_4 = PBz * dp_0.x_4_3 - PCz * dp_1.x_4_3; 
-  x_9_4 += 0.5/Zeta * 2.000000 * (ds_0.x_4_0 - ds_1.x_4_0); 
+  x_9_4 += 0.5/Zeta * 1.000000 * (ds_0.x_4_0 - ds_1.x_4_0); 
   x_4_5 = PBx * dp_0.x_5_2 - PCx * dp_1.x_5_2; 
-  x_4_5 += 0.5/Zeta * 1.000000 * (ds_0.x_5_0 - ds_1.x_5_0); 
   x_5_5 = PBy * dp_0.x_5_3 - PCy * dp_1.x_5_3; 
-  x_5_5 += 0.5/Zeta * 1.000000 * (ds_0.x_5_0 - ds_1.x_5_0); 
   x_5_5 += 0.5/Zeta * 1.000000 * (pp_0.x_3_3 - pp_1.x_3_3); 
   x_6_5 = PBx * dp_0.x_5_3 - PCx * dp_1.x_5_3; 
-  x_6_5 += 0.5/Zeta * 1.000000 * (ds_0.x_5_0 - ds_1.x_5_0); 
   x_7_5 = PBx * dp_0.x_5_1 - PCx * dp_1.x_5_1; 
-  x_7_5 += 0.5/Zeta * 2.000000 * (ds_0.x_5_0 - ds_1.x_5_0); 
+  x_7_5 += 0.5/Zeta * 1.000000 * (ds_0.x_5_0 - ds_1.x_5_0); 
   x_8_5 = PBy * dp_0.x_5_2 - PCy * dp_1.x_5_2; 
-  x_8_5 += 0.5/Zeta * 2.000000 * (ds_0.x_5_0 - ds_1.x_5_0); 
+  x_8_5 += 0.5/Zeta * 1.000000 * (ds_0.x_5_0 - ds_1.x_5_0); 
   x_8_5 += 0.5/Zeta * 1.000000 * (pp_0.x_3_2 - pp_1.x_3_2); 
   x_9_5 = PBz * dp_0.x_5_3 - PCz * dp_1.x_5_3; 
-  x_9_5 += 0.5/Zeta * 2.000000 * (ds_0.x_5_0 - ds_1.x_5_0); 
+  x_9_5 += 0.5/Zeta * 1.000000 * (ds_0.x_5_0 - ds_1.x_5_0); 
   x_9_5 += 0.5/Zeta * 1.000000 * (pp_0.x_2_3 - pp_1.x_2_3); 
   x_4_6 = PBx * dp_0.x_6_2 - PCx * dp_1.x_6_2; 
-  x_4_6 += 0.5/Zeta * 1.000000 * (ds_0.x_6_0 - ds_1.x_6_0); 
   x_4_6 += 0.5/Zeta * 1.000000 * (pp_0.x_3_2 - pp_1.x_3_2); 
   x_5_6 = PBy * dp_0.x_6_3 - PCy * dp_1.x_6_3; 
-  x_5_6 += 0.5/Zeta * 1.000000 * (ds_0.x_6_0 - ds_1.x_6_0); 
   x_6_6 = PBx * dp_0.x_6_3 - PCx * dp_1.x_6_3; 
-  x_6_6 += 0.5/Zeta * 1.000000 * (ds_0.x_6_0 - ds_1.x_6_0); 
   x_6_6 += 0.5/Zeta * 1.000000 * (pp_0.x_3_3 - pp_1.x_3_3); 
   x_7_6 = PBx * dp_0.x_6_1 - PCx * dp_1.x_6_1; 
-  x_7_6 += 0.5/Zeta * 2.000000 * (ds_0.x_6_0 - ds_1.x_6_0); 
+  x_7_6 += 0.5/Zeta * 1.000000 * (ds_0.x_6_0 - ds_1.x_6_0); 
   x_7_6 += 0.5/Zeta * 1.000000 * (pp_0.x_3_1 - pp_1.x_3_1); 
   x_8_6 = PBy * dp_0.x_6_2 - PCy * dp_1.x_6_2; 
-  x_8_6 += 0.5/Zeta * 2.000000 * (ds_0.x_6_0 - ds_1.x_6_0); 
+  x_8_6 += 0.5/Zeta * 1.000000 * (ds_0.x_6_0 - ds_1.x_6_0); 
   x_9_6 = PBz * dp_0.x_6_3 - PCz * dp_1.x_6_3; 
-  x_9_6 += 0.5/Zeta * 2.000000 * (ds_0.x_6_0 - ds_1.x_6_0); 
+  x_9_6 += 0.5/Zeta * 1.000000 * (ds_0.x_6_0 - ds_1.x_6_0); 
   x_9_6 += 0.5/Zeta * 1.000000 * (pp_0.x_1_3 - pp_1.x_1_3); 
   x_4_7 = PBx * dp_0.x_7_2 - PCx * dp_1.x_7_2; 
-  x_4_7 += 0.5/Zeta * 1.000000 * (ds_0.x_7_0 - ds_1.x_7_0); 
   x_4_7 += 0.5/Zeta * 2.000000 * (pp_0.x_1_2 - pp_1.x_1_2); 
   x_5_7 = PBy * dp_0.x_7_3 - PCy * dp_1.x_7_3; 
-  x_5_7 += 0.5/Zeta * 1.000000 * (ds_0.x_7_0 - ds_1.x_7_0); 
   x_6_7 = PBx * dp_0.x_7_3 - PCx * dp_1.x_7_3; 
-  x_6_7 += 0.5/Zeta * 1.000000 * (ds_0.x_7_0 - ds_1.x_7_0); 
   x_6_7 += 0.5/Zeta * 2.000000 * (pp_0.x_1_3 - pp_1.x_1_3); 
   x_7_7 = PBx * dp_0.x_7_1 - PCx * dp_1.x_7_1; 
-  x_7_7 += 0.5/Zeta * 2.000000 * (ds_0.x_7_0 - ds_1.x_7_0); 
+  x_7_7 += 0.5/Zeta * 1.000000 * (ds_0.x_7_0 - ds_1.x_7_0); 
   x_7_7 += 0.5/Zeta * 2.000000 * (pp_0.x_1_1 - pp_1.x_1_1); 
   x_8_7 = PBy * dp_0.x_7_2 - PCy * dp_1.x_7_2; 
-  x_8_7 += 0.5/Zeta * 2.000000 * (ds_0.x_7_0 - ds_1.x_7_0); 
+  x_8_7 += 0.5/Zeta * 1.000000 * (ds_0.x_7_0 - ds_1.x_7_0); 
   x_9_7 = PBz * dp_0.x_7_3 - PCz * dp_1.x_7_3; 
-  x_9_7 += 0.5/Zeta * 2.000000 * (ds_0.x_7_0 - ds_1.x_7_0); 
+  x_9_7 += 0.5/Zeta * 1.000000 * (ds_0.x_7_0 - ds_1.x_7_0); 
   x_4_8 = PBx * dp_0.x_8_2 - PCx * dp_1.x_8_2; 
-  x_4_8 += 0.5/Zeta * 1.000000 * (ds_0.x_8_0 - ds_1.x_8_0); 
   x_5_8 = PBy * dp_0.x_8_3 - PCy * dp_1.x_8_3; 
-  x_5_8 += 0.5/Zeta * 1.000000 * (ds_0.x_8_0 - ds_1.x_8_0); 
   x_5_8 += 0.5/Zeta * 2.000000 * (pp_0.x_2_3 - pp_1.x_2_3); 
   x_6_8 = PBx * dp_0.x_8_3 - PCx * dp_1.x_8_3; 
-  x_6_8 += 0.5/Zeta * 1.000000 * (ds_0.x_8_0 - ds_1.x_8_0); 
   x_7_8 = PBx * dp_0.x_8_1 - PCx * dp_1.x_8_1; 
-  x_7_8 += 0.5/Zeta * 2.000000 * (ds_0.x_8_0 - ds_1.x_8_0); 
+  x_7_8 += 0.5/Zeta * 1.000000 * (ds_0.x_8_0 - ds_1.x_8_0); 
   x_8_8 = PBy * dp_0.x_8_2 - PCy * dp_1.x_8_2; 
-  x_8_8 += 0.5/Zeta * 2.000000 * (ds_0.x_8_0 - ds_1.x_8_0); 
+  x_8_8 += 0.5/Zeta * 1.000000 * (ds_0.x_8_0 - ds_1.x_8_0); 
   x_8_8 += 0.5/Zeta * 2.000000 * (pp_0.x_2_2 - pp_1.x_2_2); 
   x_9_8 = PBz * dp_0.x_8_3 - PCz * dp_1.x_8_3; 
-  x_9_8 += 0.5/Zeta * 2.000000 * (ds_0.x_8_0 - ds_1.x_8_0); 
+  x_9_8 += 0.5/Zeta * 1.000000 * (ds_0.x_8_0 - ds_1.x_8_0); 
   x_4_9 = PBx * dp_0.x_9_2 - PCx * dp_1.x_9_2; 
-  x_4_9 += 0.5/Zeta * 1.000000 * (ds_0.x_9_0 - ds_1.x_9_0); 
   x_5_9 = PBy * dp_0.x_9_3 - PCy * dp_1.x_9_3; 
-  x_5_9 += 0.5/Zeta * 1.000000 * (ds_0.x_9_0 - ds_1.x_9_0); 
   x_6_9 = PBx * dp_0.x_9_3 - PCx * dp_1.x_9_3; 
-  x_6_9 += 0.5/Zeta * 1.000000 * (ds_0.x_9_0 - ds_1.x_9_0); 
   x_7_9 = PBx * dp_0.x_9_1 - PCx * dp_1.x_9_1; 
-  x_7_9 += 0.5/Zeta * 2.000000 * (ds_0.x_9_0 - ds_1.x_9_0); 
+  x_7_9 += 0.5/Zeta * 1.000000 * (ds_0.x_9_0 - ds_1.x_9_0); 
   x_8_9 = PBy * dp_0.x_9_2 - PCy * dp_1.x_9_2; 
-  x_8_9 += 0.5/Zeta * 2.000000 * (ds_0.x_9_0 - ds_1.x_9_0); 
+  x_8_9 += 0.5/Zeta * 1.000000 * (ds_0.x_9_0 - ds_1.x_9_0); 
   x_9_9 = PBz * dp_0.x_9_3 - PCz * dp_1.x_9_3; 
-  x_9_9 += 0.5/Zeta * 2.000000 * (ds_0.x_9_0 - ds_1.x_9_0); 
+  x_9_9 += 0.5/Zeta * 1.000000 * (ds_0.x_9_0 - ds_1.x_9_0); 
   x_9_9 += 0.5/Zeta * 2.000000 * (pp_0.x_3_3 - pp_1.x_3_3); 
 
  } 
