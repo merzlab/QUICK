ae0b1afce0ea2bbab242520ba9c4b1356c11cb80
Vinicius Wilian D. Cruzeiro <vwcruz@stanford.edu>
2021-10-26 15:34:03 -0700

    Wipe the entire content of the cmake folder. This folder will become a submodule in the next step.

--- cmake/ParallelizationConfig.cmake
+++
@@ -1,8 +0,0 @@
-# file with configuration that is common to both OpenMP and MPI.
-# It is included from both OpenMPConfig and MPIConfig.
-
-if(OPENMP OR MPI)
-	set(PARALLEL TRUE)
-else()
-	set(PARALLEL FALSE)
-endif()
