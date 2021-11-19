442813d61240e02a3ee0aa48b3a45e21dc0ae8dd
Madu86 <mmmskumara@gmail.com>
2021-11-19 08:32:56 -0800

    isolated temp stuff from AMD into a separate module

---
+++ src/hip/hipblas/rocblas_extras_module.f90
@@ -0,0 +1,130 @@
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! Copyright (c) 2020 Advanced Micro Devices, Inc.
+!
+! Permission is hereby granted, free of charge, to any person obtaining a copy
+! of this software and associated documentation files (the "Software"), to deal
+! in the Software without restriction, including without limitation the rights
+! to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+! copies of the Software, and to permit persons to whom the Software is
+! furnished to do so, subject to the following conditions:
+!
+! The above copyright notice and this permission notice shall be included in
+! all copies or substantial portions of the Software.
+!
+! THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+! IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+! FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+! AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+! LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+! OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+! THE SOFTWARE.
+!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+! Madu Manathunga 11/16/2021: 
+! This module contains some extra subroutines and wrappers useful for rocBLAS and
+! rocSolver. Once the AMD team put together a plugin as they say, this module can be eliminated. 
+    
+module rocblas_extras
+
+    implicit none
+
+    ! AMD - TODO: hip workaround until plugin is ready.
+    interface
+        function hipMalloc(ptr, size) &
+                result(c_int) &
+                bind(c, name = 'hipMalloc')
+            use iso_c_binding
+            implicit none
+            type(c_ptr), value :: ptr
+            integer(c_size_t), value :: size
+        end function hipMalloc
+    end interface
+
+    interface
+        function hipFree(ptr) &
+                result(c_int) &
+                bind(c, name = 'hipFree')
+            use iso_c_binding
+            implicit none
+            type(c_ptr), value :: ptr
+        end function hipFree
+    end interface
+
+    interface
+        function hipMemcpy(dst, src, size, kind) &
+                result(c_int) &
+                bind(c, name = 'hipMemcpy')
+            use iso_c_binding
+            implicit none
+            type(c_ptr), value :: dst
+            type(c_ptr), intent(in), value :: src
+            integer(c_size_t), value :: size
+            integer(c_int), value :: kind
+        end function hipMemcpy
+    end interface
+
+    interface
+        function hipMemset(dst, val, size) &
+                result(c_int) &
+                bind(c, name = 'hipMemset')
+            use iso_c_binding
+            implicit none
+            type(c_ptr), value :: dst
+            integer(c_int), value :: val
+            integer(c_size_t), value :: size
+        end function hipMemset
+    end interface
+
+    interface
+        function hipDeviceSynchronize() &
+                result(c_int) &
+                bind(c, name = 'hipDeviceSynchronize')
+            use iso_c_binding
+            implicit none
+        end function hipDeviceSynchronize
+    end interface
+
+    interface
+        function hipDeviceReset() &
+                result(c_int) &
+                bind(c, name = 'hipDeviceReset')
+            use iso_c_binding
+            implicit none
+        end function hipDeviceReset
+    end interface
+    ! AMD - TODO end
+
+contains
+
+    ! AMD: Error handling subroutines
+    subroutine HIP_CHECK(stat)
+        use iso_c_binding
+    
+        implicit none
+    
+        integer(c_int) :: stat
+    
+        if(stat /= 0) then
+            write(*,*) 'Error: hip error'
+            stop
+        end if
+    end subroutine HIP_CHECK
+    
+    subroutine ROCBLAS_CHECK(stat)
+        use iso_c_binding
+    
+        implicit none
+    
+        integer(c_int) :: stat
+    
+        if(stat /= 0) then
+            write(*,*) 'Error: rocblas error'
+            stop
+        endif
+    end subroutine ROCBLAS_CHECK
+    ! AMD: End of error handling subroutines
+
+
+end module rocblas_extras
+
