ae7db9af4dcde73f9d50c9eb76a85d83dc47d4f1
Madu86 <mmmskumara@gmail.com>
2022-07-14 07:26:36 -0700

    added new module

---
+++ src/modules/quick_molden_module.f90
@@ -0,0 +1,12 @@
+!---------------------------------------------------------------------!
+! Created by Madu Manathunga on 07/14/2022                            !
+!                                                                     !
+! Copyright (C) 2020-2021 Merz lab                                    !
+! Copyright (C) 2020-2021 Götz lab                                    !
+!                                                                     !
+! This Source Code Form is subject to the terms of the Mozilla Public !
+! License, v. 2.0. If a copy of the MPL was not distributed with this !
+! file, You can obtain one at http://mozilla.org/MPL/2.0/.            !
+!_____________________________________________________________________!
+
+#include "util.fh"
