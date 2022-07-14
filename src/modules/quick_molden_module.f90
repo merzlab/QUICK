2a262c53e5becb34cec89f18c635f1bcc0f6da20
Madushanka Manathunga <mmmskumara@gmail.com>
2022-07-14 11:35:22 -0400

    implemented more on molden exporting - updated to write coords

--- src/modules/quick_molden_module.f90
+++ src/modules/quick_molden_module.f90
@@ -16,24 +16,50 @@ module quick_molden_module
     
 contains
 
-subroutine write_molden(iMoldenFile, moldenFileName, ierr)
+subroutine write_coordinates(iMoldenFile, ierr)
+
+    use quick_molspec_module, only: quick_molspec, xyz, natom
+    use quick_constants_module, only :: symbol, BOHRS_TO_A
+    implicit none
+    integer, intent(in) :: iMoldenFile
+    integer :: i, j
+
+    ! write atomic labels and coordinates
+    write(iMoldenFile, '("[Atoms] (Ang)")')
+    do i=1,natom
+        write(iMoldenFile,'("A2,4x,I5,4x,I3,4x,F10.4,4x,F10.4,4x,F10.4")') &
+        symbol(quick_molspec%iattype(i)), i, quick_molspec%iattype(i), (xyz(j,i)*BOHRS_TO_A,j=1,3)
+    enddo
+
+end subroutine write_coordinates
+
+subroutine initialize_molden(iMoldenFile, moldenFileName, ierr)
     
+    use quick_molspec_module, only: quick_molspec, xyz, natom
+    use quick_constants_module, only :: symbol
     implicit none
     integer, intent(in) :: iMoldenFile
     character(len=*) ::  moldenFileName
     integer, intent(out) :: ierr
+    integer :: i, j
 
     ! open file
     call quick_open(iMoldenFile,moldenFileName,'U','F','R',.false.,ierr)
     CHECK_ERROR(ierr)
 
-    
+    write(iMoldenFile, '("[Molden Format]")')
+
+end subroutine initialize_molden
+
+subroutine finalize_molden(iMoldenFile, ierr)
+
+    implicit none
+    integer, intent(in) :: iMoldenFile
+    integer, intent(out) :: ierr
 
     ! close file
-    call close(moldenFileName)
-end subroutine write_molden
+    call close(iMoldenFile)
 
-
-
+end subroutine finalize_molden
 
 end module quick_molden_module
