6efc1e88d61879be7efd2353b942a58e91f61dce
Madu86 <mmmskumara@gmail.com>
2022-07-28 10:32:20 -0400

    fixed a compile time error

--- src/hip/gpu_getxc.cu
+++ src/hip/gpu_getxc.cu
@@ -6,7 +6,7 @@
 #include "gpu_work_gga_c.cu"
 #include "gpu_work_lda.cu"
 
-static __constant__ gpu_simulation_type devSim_dft;
+__constant__ gpu_simulation_type devSim_dft;
 
 #if defined DEBUG || defined DEBUGTIME
 static float totTime;
