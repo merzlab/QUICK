21c5135a65d151817e042a2ff6eec617a490f206
Madu86 <mmmskumara@gmail.com>
2023-01-10 10:23:41 -0800

    disabling useless legacy kernels

--- src/cuda/gpu_get2e.cu
+++ src/cuda/gpu_get2e.cu
@@ -255,6 +255,7 @@ texture <int2, cudaTextureType1D, cudaReadModeElementType> tex_Xcoeff;
 #include "gpu_eri_assembler_spdf_8.h"
 #include "gpu_get2e_subs.h"
 
+/*
 #undef int_spd
 #undef int_spdf
 #undef int_spdf2
@@ -280,7 +281,7 @@ texture <int2, cudaTextureType1D, cudaReadModeElementType> tex_Xcoeff;
 #undef int_spdf9
 #define int_spdf10
 #include "gpu_get2e_subs.h"
-
+*/
 #endif
 
 #undef int_spd
@@ -479,7 +480,7 @@ texture <int2, cudaTextureType1D, cudaReadModeElementType> tex_Xcoeff;
 #undef int_spdf10
 #include "gpu_get2e_subs.h"
 
-
+/*
 #undef int_spd
 #undef int_spdf
 #undef int_spdf2
@@ -505,7 +506,10 @@ texture <int2, cudaTextureType1D, cudaReadModeElementType> tex_Xcoeff;
 #undef int_spdf9
 #define int_spdf10
 #include "gpu_get2e_subs.h"
+<<<<<<< HEAD
 
+=======
+*/
 #endif
 
 #undef int_spd
