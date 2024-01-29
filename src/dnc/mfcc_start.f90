e2c801ad9ca9c0cfee8da36f95bb52144d87084d
d_kaliakin <dskalyakin@gmail.com>
2024-01-29 12:44:05 -0500

    Recovered 42% of mfcc_start.f90 module

--- src/dnc/mfcc_start.f90
+++ src/dnc/mfcc_start.f90
@@ -8,13 +8,18 @@ subroutine mfcc(natomsaved)
    implicit none
    integer xiaoconnect(100,100)
    integer :: i,j,j1,j2,j3,number,mm,nn,kk
+   integer :: mmm,nnn,nnnn,k
    character*6,allocatable:: sn(:)             ! series no.
    double precision,allocatable::coord(:,:)    ! cooridnates
    integer,allocatable::class(:),ttnumber(:)   ! class and residue number
    character*4,allocatable::atomname(:)        ! atom name
    character*3,allocatable::residue(:)         ! residue name
-   integer natomsaved
+   integer natomsaved,npmfcc
    integer,allocatable::mselectC(:),mselectN(:),mselectCA(:) 
+   real(8)::xx,yy,zz,ym,zm
+   integer :: mfccatom(50),mfcccharge(50)
+   integer :: mfccatomcap(50),mfccchargecap(50)
+   integer :: mspin(50)
 
 !   double precision :: mfcccord(:,:,:)
 !   integer :: selectC(:), selectCA(:)
@@ -58,7 +63,12 @@ subroutine mfcc(natomsaved)
 
    write(*,*) "Processed PDB file"
 
-! Confirm reading of PDB
+! Confirm reading of residue
+! do i=1,number
+!  write(*,*) residue(i)
+! enddo
+
+! Confirm reading of atomname
 ! do i=1,number
 !  write(*,*) atomname(i)
 ! enddo
@@ -71,17 +81,18 @@ subroutine mfcc(natomsaved)
 ! Number of fragments
  npmfcc=class(number)
 
+  write(*,*) "npmfcc=", npmfcc
   write(*,*) "Assigned number of fragments"
 
 ! Assign zero values for initialization of MFCC
 ! Make multiplicity equal to one for all fragments
 
-   do i=1,np
+   do i=1,npmfcc
     mfcccharge(i)=0
     mfccatom(i)=0
     mfccchargecap(i)=0
     mfccatomcap(i)=0
-    spin(i)=1
+    mspin(i)=1
   enddo
 
  write(*,*) "Initialiazed MFCC arguments"
@@ -127,4 +138,144 @@ subroutine mfcc(natomsaved)
       enddo
   enddo
 
+! Initiate xyzchange
+
+ call xyzchange(coord(1,mm),coord(2,mm),coord(3,mm), &
+   coord(1,nn),coord(2,nn),coord(3,nn),xx,yy,zz)     
+
+ write(*,*) "first call xyzchange output" 
+ write(*,*)'H ',xx,yy,zz
+
+ mfccatomxiao(mm,1)='H '
+
+ mfcccord(1,mm,1)=xx
+ mfcccord(2,mm,1)=yy
+ mfcccord(3,mm,1)=zz
+
+ mfccatom(1)=mm
+
+ mfccstart(1)=1
+ mfccfinal(1)=mm-1
+
+ matomstart(1)=1
+ matomfinal(1)=mm-1
+
+  do k=2,npmfcc-1
+ 
+   mm=mselectN(k-1)
+   nn=mselectC(k+1)
+   mmm=mselectCA(k-1)
+   nnn=mselectCA(k+1)
+   nnnn=mselectC(k-2)
+   !write(*,*) residue(mselectN(k-1)), 'residue(mselectN(k-1))'
+   if(residue(mselectN(k-1)).ne.'PRO')then
+    call xyzchange(coord(1,mm),coord(2,mm),coord(3,mm), &
+    coord(1,mmm),coord(2,mmm),coord(3,mmm),xx,ym,zm)    
+    write(*,*) 'second call xyzchange output'
+    write(*,*)'H ',xx,ym,zm
+
+    mfccatomxiao(1,k)='H '
+
+    mfcccord(1,1,k)=xx
+    mfcccord(2,1,k)=ym
+    mfcccord(3,1,k)=zm
+
+    mfccatom(k)=nn-mmm+1+1
+
+    mfccstart(k)=2
+    mfccfinal(k)=nn-mmm+1
+
+    matomstart(k)=mmm
+    matomfinal(k)=nn-1
+
+    do kk=mmm,nn-1
+      write(*,*)atomname(kk)(2:2)//' ',(coord(j,kk),j=1,3)
+      mfccatomxiao(kk-mmm+2,k)=atomname(kk)(2:2)//' '
+      do j=1,3
+        mfcccord(j,kk-mmm+2,k)=coord(j,kk)
+      enddo
+    enddo
+
+   call xyzchange(coord(1,nn),coord(2,nn),coord(3,nn), &
+   coord(1,nnn),coord(2,nnn),coord(3,nnn),xx,ym,zm)
+   write(*,*) 'third call xyzchange output'
+   write(*,*)'H ',xx,ym,zm
+
+   mfccatomxiao(nn-mmm+2,k)='H '
+
+   mfcccord(1,nn-mmm+2,k)=xx
+   mfcccord(2,nn-mmm+2,k)=ym
+   mfcccord(3,nn-mmm+2,k)=zm
+      
+    else
+
+   call Nxyzchange(coord(1,nnnn),coord(2,nnnn),coord(3,nnnn), &
+   coord(1,mm),coord(2,mm),coord(3,mm),xx,ym,zm)       
+   write(*,*)'H ',xx,ym,zm
+
+   mfccatomxiao(1,k)='H '
+
+   mfcccord(1,1,k)=xx
+   mfcccord(2,1,k)=ym
+   mfcccord(3,1,k)=zm
+
+   mfccatom(k)=nn-mm+1+1
+
+   mfccstart(k)=2
+   mfccfinal(k)=nn-mm+1
+
+   matomstart(k)=mm
+   matomfinal(k)=nn-1
+
+   do kk=mm,nn-1
+      write(*,*)atomname(kk)(2:2)//' ',(coord(j,kk),j=1,3)
+
+      mfccatomxiao(kk-mm+2,k)=atomname(kk)(2:2)//' '
+      do j=1,3
+        mfcccord(j,kk-mm+2,k)=coord(j,kk)
+      enddo
+
+   enddo
+
+   call xyzchange(coord(1,nn),coord(2,nn),coord(3,nn), &
+   coord(1,nnn),coord(2,nnn),coord(3,nnn),xx,ym,zm)
+   write(*,*) 'PROline call xyzchange output'
+   write(*,*)'H ',xx,ym,zm
+
+   mfccatomxiao(nn-mm+2,k)='H '
+
+   mfcccord(1,nn-mm+2,k)=xx
+   mfcccord(2,nn-mm+2,k)=ym
+   mfcccord(3,nn-mm+2,k)=zm
+
+  endif
+  enddo
+end
+
+subroutine xyzchange(xold,yold,zold,xzero,yzero,zzero, &
+  xnew,ynew,znew)
+
+  implicit none
+  real(8)::grad,xold,yold,zold,xzero,yzero,zzero
+  real(8)::xnew,ynew,znew
+
+  grad=dsqrt(1.09d0**2/((xold-xzero)**2+(yold-yzero)**2 &
+  +(zold-zzero)**2))
+  xnew=xzero+grad*(xold-xzero)
+  ynew=yzero+grad*(yold-yzero)
+  znew=zzero+grad*(zold-zzero)
+end
+
+subroutine Nxyzchange(xold,yold,zold,xzero,yzero,zzero, &
+  xnew,ynew,znew)
+
+  implicit none
+  real(8)::grad,xold,yold,zold,xzero,yzero,zzero
+  real(8)::xnew,ynew,znew
+
+  grad=dsqrt(1.01d0**2/((xold-xzero)**2+(yold-yzero)**2 &
+  +(zold-zzero)**2))
+  xnew=xzero+grad*(xold-xzero)
+  ynew=yzero+grad*(yold-yzero)
+  znew=zzero+grad*(zold-zzero)
 end
