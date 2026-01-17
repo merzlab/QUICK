04902c416f7cfc62903fa0e64a6b040729b6f51a
Vikrant Tripathy <vikranttripathy65@gmail.com>
2026-01-16 19:21:41 -0800

    calling a subroutine for diagonalization in SCF routine

---
+++ src/subs/MatDiag.f90
@@ -0,0 +1,36 @@
+!
+!       MatDiag.f90
+!
+!       Created by Vikrant Tripathy on 01/16/26.
+!
+!-----------------------------------------------------------
+
+subroutine MatDiag(Mat,Eig,Vec,Cutoff,idegen,V2,nbasis)
+
+    double precision :: Mat(nbasis,nbasis), Eig(nbasis), Vec(nbasis,nbasis)
+    double precision :: V2(3, nbasis), idegen(nbasis)
+    integer :: nbasis,IERROR
+
+#if defined(HIP) || defined(HIP_MPIV)                                          
+#if defined(WITH_MAGMA)
+           call magmaDIAG(nbasis, Mat, Eig, Vec, IERROR)
+#elif defined(WITH_ROCSOLVER) 
+           call rocDIAG(nbasis, Mat, Eig, Vec, IERROR)
+#else
+#if defined(LAPACK) || defined(MKL)
+           call DIAGMKL(nbasis, Mat, Eig, Vec, IERROR)
+#else
+           call DIAG(nbasis, Mat, nbasis, Cutoff, V2, Eig, idegen, Vec, IERROR)
+#endif
+#endif
+#elif defined(CUDA) || defined(CUDA_MPIV)
+           call CUDA_DIAG(Mat, Eig, Vec, nbasis)    
+#else  
+#if defined(LAPACK) || defined(MKL)                                            
+           call DIAGMKL(nbasis, Mat, Eig, Vec, IERROR)
+#else
+           call DIAG(nbasis, Mat, nbasis, Cutoff, V2, Eig, idegen, Vec, IERROR)
+#endif
+#endif
+
+end subroutine MatDiag
