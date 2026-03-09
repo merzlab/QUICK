8c358132bc3646a5959d9c34bf62de44dbbd1d5a
Kurt A. O'Hearn <1138669+ohearnk@users.noreply.github.com>
2026-03-09 13:14:20 -0400

    Fix Fortran compilation error (source line truncations with older compilers). Default to SG-1 grids (API).

--- src/modules/quick_method_module.f90
+++ src/modules/quick_method_module.f90
@@ -952,7 +952,7 @@ module quick_method_module
             self%DIVCON = .false.      ! Div&Con
 
             self%ifragbasis = 1        ! =2.residue basis,=1.atom basis(DEFUALT),=3 non-h atom basis
-            self%iSG = 3               ! =0. SG0, =1. (DEFAULT)
+            self%iSG = 1               ! =0. SG0, =1. (DEFAULT), =2. SG2, =3. SG3
             self%MFCC = .false.        ! MFCC
 
             self%iscf = 200
