c9c249031186374e7cc00921898243a1316e60d9
Vikrant Tripathy <vikranttripathy65@gmail.com>
2026-07-14 18:03:04 -0700

    CUTOFF keyword is now detected using found_keyword

--- src/modules/quick_method_module.f90
+++ src/modules/quick_method_module.f90
@@ -769,7 +769,7 @@ module quick_method_module
             endif
 
             ! 2e-cutoff
-            if (index(keywd,'CUTOFF') /= 0) then
+            if (found_keyword(keywd,'CUTOFF')) then
                 call read(keywd, 'CUTOFF', self%integralCutoff)
                 self%primLimit=self%integralCutoff*1.0d-1
             endif
