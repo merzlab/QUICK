671d513925ac7ca894d584dcbc044ca16854d926
michaelyxsun <michaelyxsun@gmail.com>
2026-08-10 17:36:52 -0700

    move lowdin orthogonalization to separate subroutine and use natural orbitals as test

--- src/modules/quick_scf_operator_module.f90
+++ src/modules/quick_scf_operator_module.f90
@@ -61,6 +61,8 @@ contains
      double precision :: Sum2Mat
      double precision :: tmp2d(nbasis, nbasis)
      double precision :: tmp_eval(nbasis), tmp_evec(nbasis, nbasis), tmp_hold(nbasis, nbasis)
+     double precision :: tmp_sqrtS(nbasis, nbasis), tmp_sqrtSinv(nbasis, nbasis)
+     integer :: nocc
 #endif
 #ifdef MPIV
      integer ierror
@@ -80,6 +82,7 @@ contains
      if (quick_method%usecuest) then
         firstiter = quick_qm_struct%co(1, 1) == 0
         hasK = quick_method%x_hybrid_coeff /= 0.0d0
+        nocc = quick_molspec%nelec/2
      endif
 #endif
   
@@ -160,39 +163,50 @@ contains
         call cuest_debuglog_PriSym(nbasis, quick_qm_struct%dense, "F12.7")
         call cuest_debuglog("====== end guess density ======")
 #endif
-        quick_qm_struct%co = 0.0d0
-        call mat_uut_eig_r(nbasis, quick_molspec%nelec/2, quick_qm_struct%dense/2.0d0, quick_qm_struct%co)
 
 #ifdef CUESTDEBUG
         ! --------------------------------------
         ! print eigenvalues of S^{1/2}PS^{1/2} !
         ! --------------------------------------
 
-        tmp_hold = 0.0d0
-        tmp_eval = 0.0d0
-        tmp_evec = 0.0d0
-        call MAT_DIAG(quick_qm_struct%s, nbasis, nbasis, tmp_eval, tmp_evec)
-        ! S = UAU^T = tmp_evec*(tmp_hold)^2*tmp_evec^T
-        do j=1, nbasis
-            if (tmp_eval(j) < 0.0d0) then
-               tmp_hold(j,j) = 0
-            else
-               tmp_hold(j,j) = sqrt(tmp_eval(j))
-            endif
-        enddo
-        ! tmp2d = U*A^{1/2} = tmp_evec*tmp_hold
-        call MAT_DGEMM('n', 'n', nbasis, nbasis, nbasis, 1.0d0, tmp_evec, nbasis, tmp_hold, nbasis, 0.0d0, tmp2d, nbasis)
-        ! tmp_hold = S^{1/2} = (U*A^{1/2})*U^T = tmp2d*tmp_evec
-        call MAT_DGEMM('n', 't', nbasis, nbasis, nbasis, 1.0d0, tmp2d, nbasis, tmp_evec, nbasis, 0.0d0, tmp_hold, nbasis)
-        ! tmp_evec = S^{1/2}P = tmp_hold*(%dense)
-        call MAT_DGEMM('n', 'n', nbasis, nbasis, nbasis, 1.0d0, tmp_hold, nbasis, quick_qm_struct%dense/2.0d0, nbasis, 0.0d0, tmp_evec, nbasis)
-        ! tmp2d = (S^{1/2}P)S^{1/2} = tmp_evec*tmp_hold
-        call MAT_DGEMM('n', 'n', nbasis, nbasis, nbasis, 1.0d0, tmp_evec, nbasis, tmp_hold, nbasis, 0.0d0, tmp2d, nbasis)
+        ! tmp_hold = 0.0d0
+        ! tmp_eval = 0.0d0
+        ! tmp_evec = 0.0d0
+        ! call MAT_DIAG(quick_qm_struct%s, nbasis, nbasis, tmp_eval, tmp_evec)
+        ! ! S = UAU^T = tmp_evec*(tmp_hold)^2*tmp_evec^T
+        ! do j=1, nbasis
+        !     if (tmp_eval(j) < 0.0d0) then
+        !        tmp_hold(j,j) = 0
+        !     else
+        !        tmp_hold(j,j) = sqrt(tmp_eval(j))
+        !     endif
+        ! enddo
+        ! ! tmp2d = U*A^{1/2} = tmp_evec*tmp_hold
+        ! call MAT_DGEMM('n', 'n', nbasis, nbasis, nbasis, 1.0d0, tmp_evec, nbasis, tmp_hold, nbasis, 0.0d0, tmp2d, nbasis)
+        ! ! tmp_hold = S^{1/2} = (U*A^{1/2})*U^T = tmp2d*tmp_evec
+        ! call MAT_DGEMM('n', 't', nbasis, nbasis, nbasis, 1.0d0, tmp2d, nbasis, tmp_evec, nbasis, 0.0d0, tmp_hold, nbasis)
+        ! ! tmp_evec = S^{1/2}P = tmp_hold*(%dense)
+        ! call MAT_DGEMM('n', 'n', nbasis, nbasis, nbasis, 1.0d0, tmp_hold, nbasis, quick_qm_struct%dense/2.0d0, nbasis, 0.0d0, tmp_evec, nbasis)
+        ! ! tmp2d = (S^{1/2}P)S^{1/2} = tmp_evec*tmp_hold
+        ! call MAT_DGEMM('n', 'n', nbasis, nbasis, nbasis, 1.0d0, tmp_evec, nbasis, tmp_hold, nbasis, 0.0d0, tmp2d, nbasis)
 
-        call MAT_DIAG(tmp2d, nbasis, nbasis, tmp_eval, tmp_evec)
+        ! tmp_hold = S^{1/2}PS^{1/2}
+        call lowdin_orth(nbasis, quick_qm_struct%s, quick_qm_struct%dense/2.0d0, tmp_sqrtS, .true., tmp_sqrtSinv, tmp_hold)
+
+        call MAT_DIAG(tmp_hold, nbasis, nbasis, tmp_eval, tmp_evec)
         call cuest_debuglog("<<<<<<<< eigenvalues of S^{1/2}PS^{1/2} <<<<<<<<")
         call cuest_debuglog_PriD1D(tmp_eval, nbasis, "F12.7")
         call cuest_debuglog(">>>>>> end eigenvalues of S^{1/2}PS^{1/2} >>>>>>")
+
+        ! tmp2d = C'
+        call mat_uut_eig_r(nbasis, quick_molspec%nelec/2, tmp_hold, tmp2d)
+        quick_qm_struct%co = 0.0d0
+        ! %co = S^{-1/2}C' = C
+        tmp_hold = matmul(tmp_sqrtSinv, tmp2d)
+        quick_qm_struct%co(:, 1:nocc) = tmp_hold(:, 1:nocc)
+#else
+        quick_qm_struct%co = 0.0d0
+        call mat_uut_eig_r(nbasis, quick_molspec%nelec/2, quick_qm_struct%dense/2.0d0, quick_qm_struct%co)
 #endif
 
 #ifdef CUESTDEBUG
